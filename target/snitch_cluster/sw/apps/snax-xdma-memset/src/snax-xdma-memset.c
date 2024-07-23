4387e54df72dca906af78fc345319e1f90dfc67b
Yunhao Deng <yunhao.deng@kuleuven.be>
2024-07-23 13:42:28 +0200

    chore: Refactor xdma error handling

--- target/snitch_cluster/sw/apps/snax-xdma-memset/src/snax-xdma-memset.c
+++ target/snitch_cluster/sw/apps/snax-xdma-memset/src/snax-xdma-memset.c
@@ -28,6 +28,7 @@ int main() {
         // The xdma core is the last compute core in the cluster
         if (xdma_memcpy_1d(tcdm_0, tcdm_0, 0x4000 * sizeof(uint8_t)) != 0) {
             printf("Error in xdma agu configuration\n");
+            err++;
         } else {
             printf("The xdma agu is configured\n");
         }
@@ -35,20 +36,27 @@ int main() {
         uint32_t ext_param[1] = {0xFFFFFFFF};
         if (xdma_enable_dst_ext(0, ext_param) != 0) {
             printf("Error in enabling xdma extension 0\n");
+            err++;
         } else {
             printf("The xdma extension 0 is enabled\n");
         }
 
         if (xdma_disable_dst_ext(1) != 0) {
             printf("Error in disabling xdma extension 1\n");
+            err++;
         } else {
             printf("The xdma extension 1 is disabled\n");
         }
 
         if (xdma_disable_dst_ext(2) != 0) {
             printf("Error in disabling xdma extension 2\n");
+            err++;
         } else {
             printf("The xdma extension 2 is disabled\n");
+        }
+
+        if (err != 0) {
+            return err;
         }
 
         int task_id = xdma_start();
@@ -62,9 +70,8 @@ int main() {
         // Check the data
         for (int i = 0; i < 0x4000; i++) {
             if (tcdm_0[i] != 0xFF) {
-                printf("Error in memset\n");
-                err = 1;
-                break;
+                printf("The memset of 0KB - 16KB is not correct\n");
+                return -1;
             }
         }
         printf("The memset of 0KB - 16KB is correct\n");
@@ -73,6 +80,7 @@ int main() {
         if (xdma_memcpy_1d(tcdm_0, tcdm_0 + 0x1000 * sizeof(uint8_t),
                            0x2000 * sizeof(uint8_t)) != 0) {
             printf("Error in xdma agu configuration\n");
+            err++;
         } else {
             printf("The xdma agu is configured\n");
         }
@@ -80,8 +88,13 @@ int main() {
         ext_param[0] = 0;
         if (xdma_enable_dst_ext(0, ext_param) != 0) {
             printf("Error in enabling xdma extension 0\n");
+            err++;
         } else {
             printf("The xdma extension 0 is enabled\n");
+        }
+
+        if (err != 0) {
+            return err;
         }
 
         task_id = xdma_start();
@@ -96,31 +109,26 @@ int main() {
         for (int i = 0; i < 0x1000; i++) {
             if (tcdm_0[i] != 0xFF) {
                 printf("Error in memset (region 0)\n");
-                err = 1;
-                break;
+                return -1;
             }
         }
         for (int i = 0x1000; i < 0x3000; i++) {
             if (tcdm_0[i] != 0x00) {
-                printf("Error in memset (region 1)\n");
-                err = 1;
-                break;
+                printf("The memset is incorrect (region 1)\n");
+                return -1;
             }
         }
         for (int i = 0x3000; i < 0x4000; i++) {
             if (tcdm_0[i] != 0xFF) {
-                printf("Error in memset (region 2)\n");
-                err = 1;
-                break;
+                printf("The memset is incorrect (region 2)\n");
+                return -1;
             }
         }
         printf("The memset of 4KB - 12KB is correct\n");
 
     } else {
-        printf("Core %d is not xdma core, so returning 0. \n",
-               snrt_cluster_core_idx());
-        return 0;
+        printf("Core %d is not xdma core. \n", snrt_cluster_core_idx());
     }
 
-    return err;
+    return 0;
 }
