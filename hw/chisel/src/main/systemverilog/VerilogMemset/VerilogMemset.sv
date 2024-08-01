3058671730c5d32d87cc5b695d7a48adf121f032
Yunhao Deng <yunhao.deng@kuleuven.be>
2024-08-01 21:51:32 +0200

    Reformat sv code

--- hw/chisel/src/main/systemverilog/VerilogMemset/VerilogMemset.sv
+++ hw/chisel/src/main/systemverilog/VerilogMemset/VerilogMemset.sv
@@ -1,17 +1,17 @@
 module VerilogMemset #(
-    userCsrNum = 1,
-    dataWidth = 512
+    parameter int userCsrNum = 1,
+    parameter int dataWidth = 512
 ) (
-    input logic clk, 
-    input logic rst_n, 
-    output logic ext_data_i_ready, 
+    input logic clk,
+    input logic rst_n,
+    output logic ext_data_i_ready,
     input logic ext_data_i_valid,
     input logic [dataWidth-1:0] ext_data_i_bits,
-    input logic ext_data_o_ready, 
+    input logic ext_data_o_ready,
     output logic ext_data_o_valid,
     output logic [dataWidth-1:0] ext_data_o_bits,
-    input logic [31:0]ext_csr_i_0, 
-    input logic ext_start_i, 
+    input logic [31:0]ext_csr_i_0,
+    input logic ext_start_i,
     output logic ext_busy_o
 );
 
@@ -22,11 +22,11 @@ module VerilogMemset #(
 
     genvar i;
     generate
+    begin:genmemset_vector
         for(i = 0; i < dataWidth/8; i = i + 1) begin
             assign ext_data_o_bits[i*8 +: 8] = memset_data;
         end
+    end
     endgenerate
-
     assign ext_busy_o = 0;
-    
 endmodule
