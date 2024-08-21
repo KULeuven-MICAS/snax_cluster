5b81a1fa4aa20ae542138c41506678675673d2ec
rgantonio <ryan.antonio@esat.kuleuven.be>
2024-08-21 13:09:13 +0200

    sw: format datagen

--- target/snitch_cluster/sw/apps/snax-hypercorex/char-recog/data/datagen.py
+++ target/snitch_cluster/sw/apps/snax-hypercorex/char-recog/data/datagen.py
@@ -6,25 +6,23 @@
 #
 # Ryan Antonio <ryan.antonio@esat.kuleuven.be>
 
-from cgi import test
 import sys
-import argparse
 import numpy as np
 import os
 import subprocess
 
-#-----------------------
+# -----------------------
 # Add data utility path
-#-----------------------
+# -----------------------
 sys.path.append(os.path.join(os.path.dirname(__file__),
                 "../../../../../../../util/sim/"))
 from data_utils import format_scalar_definition, \
     format_vector_definition  # noqa: E402
 
-#-----------------------
+# -----------------------
 # Add hypercorex utility paths
-#-----------------------
-bender_command = subprocess.run(['bender', 'path', 'hypercorex'], \
+# -----------------------
+bender_command = subprocess.run(['bender', 'path', 'hypercorex'],
                                 capture_output=True, text=True)
 hypercorex_path = bender_command.stdout.strip()
 
@@ -34,9 +32,8 @@ char_recog_dataset_path = hypercorex_path + \
 sys.path.append(hypercorex_path + "/hdc_exp/")
 sys.path.append(hypercorex_path + "/sw/")
 
-from hdc_util import gen_ca90_im_set # noqa: E402
+from hdc_util import gen_ca90_im_set  # noqa: E402
 from char_recog import (  # noqa: E402
-    train_char_recog,
     convert_to_data_indices,
     char_recog_dataset,
 )
@@ -48,9 +45,10 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 train_asm_path = current_directory + "/train_char_recog_asm.txt"
 test_asm_path = current_directory + "/test_char_recog_asm.txt"
 
-#-----------------------
+# -----------------------
 # Some useful functions
-#-----------------------
+# -----------------------
+
 
 # Convert from list to binary value
 def hvlist2num(hv_list):
@@ -63,9 +61,9 @@ def hvlist2num(hv_list):
     return hv_num
 
 
-#-----------------------
+# -----------------------
 # Working fixed parameters
-#-----------------------
+# -----------------------
 
 # Hard parameters
 REG_DATA_WIDTH = 32
@@ -82,12 +80,14 @@ HV_DIM = 512
 NUM_TOT_IM = 1024
 NUM_PER_IM_BANK = 128
 
+
 # Main function to generate data
 def main():
-    
     # Extract instructions for training and testing
-    train_code_list, train_control_code_list = compile_hypercorex_asm(train_asm_path)
-    test_code_list, test_control_code_list = compile_hypercorex_asm(test_asm_path)
+    train_code_list, train_control_code_list = \
+        compile_hypercorex_asm(train_asm_path)
+    test_code_list, test_control_code_list = \
+        compile_hypercorex_asm(test_asm_path)
 
     # Convert each instruction to integers for input
     for i in range(len(train_code_list)):
@@ -103,7 +103,7 @@ def main():
         num_total_im=NUM_TOT_IM,
         num_per_im_bank=NUM_PER_IM_BANK,
     )
-    
+
     # Extract data set
     dataset = char_recog_dataset(char_recog_dataset_path)
     # Convert data set to index-based encoding
@@ -113,17 +113,26 @@ def main():
     flat_char_dataset = sum(index_based_dataset, [])
 
     # Generating strings to print
-    num_classes_str = format_scalar_definition('uint32_t', 'num_classes', NUM_CLASSES)
-    num_test_items_str = format_scalar_definition('uint32_t', 'num_test_items', NUM_ITEMS_TO_TEST)
-    num_features_str = format_scalar_definition('uint32_t', 'num_features', TOTAL_PIXEL_FEATURES)
-    train_inst_str = format_vector_definition('uint32_t', 'train_inst_code', train_code_list)
-    test_inst_str = format_vector_definition('uint32_t', 'test_inst_code', test_code_list)
-    im_seed_list_str = format_vector_definition('uint32_t', 'im_seed_list', im_seed_list)
-    char_data_str = format_vector_definition('uint64_t', 'char_data', flat_char_dataset)
+    num_classes_str = format_scalar_definition(
+        'uint32_t', 'num_classes', NUM_CLASSES)
+    num_test_items_str = format_scalar_definition(
+        'uint32_t', 'num_test_items', NUM_ITEMS_TO_TEST)
+    num_features_str = format_scalar_definition(
+        'uint32_t', 'num_features', TOTAL_PIXEL_FEATURES)
+    train_inst_str = format_vector_definition(
+        'uint32_t', 'train_inst_code', train_code_list)
+    test_inst_str = format_vector_definition(
+        'uint32_t', 'test_inst_code', test_code_list)
+    im_seed_list_str = format_vector_definition(
+        'uint32_t', 'im_seed_list', im_seed_list)
+    char_data_str = format_vector_definition(
+        'uint64_t', 'char_data', flat_char_dataset)
 
     # Preparing string to load
-    f_str = '\n\n'.join([num_classes_str, num_test_items_str, num_features_str, \
-                         train_inst_str, test_inst_str, im_seed_list_str, char_data_str])
+    f_str = '\n\n'.join([num_classes_str, num_test_items_str,
+                         num_features_str, train_inst_str,
+                         test_inst_str, im_seed_list_str,
+                         char_data_str])
     f_str += '\n'
 
     # Write to stdout
