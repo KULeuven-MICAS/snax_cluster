3701cfb33dbc26fbc081ceb938d4f45462eee759
Konste11ation <fanchen.kong@outlook.be>
2024-08-27 18:10:38 +0200

    1. Use the clint to format c code 2. Format the datagen.py 3. Fix the bug in snax-xdma-run.yaml on path

--- target/snitch_cluster/sw/apps/snax-xdma-maxpool/data/datagen.py
+++ target/snitch_cluster/sw/apps/snax-xdma-maxpool/data/datagen.py
@@ -7,10 +7,12 @@ import sys
 import os
 
 # Add data utility path
-sys.path.append(os.path.join(os.path.dirname(__file__), "../../../../../../util/sim/"))
+sys.path.append(os.path.join(os.path.dirname(
+    __file__), "../../../../../../util/sim/"))
 from data_utils import format_scalar_definition, format_vector_definition  # noqa E402
-from snax_utils import max_pooling
+from snax_utils import max_pooling  # noqa E402
 np.random.seed(42)
+
 
 # Add stdint.h header
 def emit_header_file(**kwargs):
@@ -25,28 +27,38 @@ def emit_data(**kwargs):
     MAX = 127
 
     data_str = ""
-    data_str += format_scalar_definition("int8_t","H",kwargs["H"]) + "\n"
-    data_str += format_scalar_definition("int8_t","W",kwargs["W"]) + "\n"
-    data_str += format_scalar_definition("int8_t","Cin",kwargs["Cin"]) + "\n"
-    data_str += format_scalar_definition("int8_t","Kh",kwargs["Kh"]) + "\n"
-    data_str += format_scalar_definition("int8_t","Kw",kwargs["Kw"]) + "\n"
-    data_str += format_scalar_definition("int8_t","pad_h",kwargs["pad_h"]) + "\n"
-    data_str += format_scalar_definition("int8_t","pad_w",kwargs["pad_w"]) + "\n"
-    data_str += format_scalar_definition("int8_t","stride_h",kwargs["stride_h"]) + "\n"
-    data_str += format_scalar_definition("int8_t","stride_w",kwargs["stride_w"]) + "\n"
-
-
+    data_str += format_scalar_definition("int8_t",
+                                         "H",
+                                         kwargs["H"]) + "\n"
+    data_str += format_scalar_definition("int8_t",
+                                         "W",
+                                         kwargs["W"]) + "\n"
+    data_str += format_scalar_definition("int8_t",
+                                         "Cin",
+                                         kwargs["Cin"]) + "\n"
+    data_str += format_scalar_definition("int8_t",
+                                         "Kh",
+                                         kwargs["Kh"]) + "\n"
+    data_str += format_scalar_definition("int8_t", "Kw", kwargs["Kw"]) + "\n"
+    data_str += format_scalar_definition("int8_t",
+                                         "pad_h", kwargs["pad_h"]) + "\n"
+    data_str += format_scalar_definition("int8_t",
+                                         "pad_w", kwargs["pad_w"]) + "\n"
+    data_str += format_scalar_definition("int8_t",
+                                         "stride_h", kwargs["stride_h"]) + "\n"
+    data_str += format_scalar_definition("int8_t",
+                                         "stride_w", kwargs["stride_w"]) + "\n"
     padded_h = kwargs["H"] + 2 * kwargs["pad_h"]
     padded_w = kwargs["W"] + 2 * kwargs["pad_w"]
-    out_h = (kwargs["H"] + 2 * kwargs["pad_h"] - kwargs["Kh"]) // kwargs["stride_h"] + 1
-    out_w = (kwargs["W"] + 2 * kwargs["pad_w"] - kwargs["Kw"]) // kwargs["stride_w"] + 1
+    out_h = (kwargs["H"] + 2 * kwargs["pad_h"] -
+             kwargs["Kh"]) // kwargs["stride_h"] + 1
+    out_w = (kwargs["W"] + 2 * kwargs["pad_w"] -
+             kwargs["Kw"]) // kwargs["stride_w"] + 1
 
-    data_str += format_scalar_definition("int8_t","out_H",out_h) + "\n"
-    data_str += format_scalar_definition("int8_t","out_W",out_w) + "\n"
-    data_str += format_scalar_definition("int8_t","padded_H",padded_h) + "\n"
-    data_str += format_scalar_definition("int8_t","padded_W",padded_w) + "\n"
-
-
+    data_str += format_scalar_definition("int8_t", "out_H", out_h) + "\n"
+    data_str += format_scalar_definition("int8_t", "out_W", out_w) + "\n"
+    data_str += format_scalar_definition("int8_t", "padded_H", padded_h) + "\n"
+    data_str += format_scalar_definition("int8_t", "padded_W", padded_w) + "\n"
 
     # Generating random input data vector
     data_in = np.random.randint(
@@ -73,13 +85,14 @@ def emit_data(**kwargs):
         kwargs["pad_h"],
         "HWC",
     )
-    data_str += format_vector_definition("int8_t", "padded_data_in", padded_data_in.reshape(-1)) + "\n"
-    data_str += format_vector_definition("int8_t", "golden_data_out", c_golden.reshape(-1)) + "\n"
+    data_str += format_vector_definition("int8_t",
+                                         "padded_data_in",
+                                         padded_data_in.reshape(-1)) + "\n"
+    data_str += format_vector_definition("int8_t",
+                                         "golden_data_out",
+                                         c_golden.reshape(-1)) + "\n"
 
     return data_str
-
-
-
 
 
 def main():
@@ -100,7 +113,6 @@ def main():
 
     # Emit header file
     print(emit_header_file(**param))
-
 
 
 if __name__ == "__main__":
