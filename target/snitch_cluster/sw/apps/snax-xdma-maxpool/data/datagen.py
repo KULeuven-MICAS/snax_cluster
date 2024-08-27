1b645fd51a7fb800a458a39aa1094647be7fad7c
Konste11ation <fanchen.kong@outlook.be>
2024-08-27 18:44:02 +0200

    1. Add the license header in datagen.py

--- target/snitch_cluster/sw/apps/snax-xdma-maxpool/data/datagen.py
+++ target/snitch_cluster/sw/apps/snax-xdma-maxpool/data/datagen.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+
+# Copyright 2024 KU Leuven.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Fanchen Kong <fanchen.kong@kuleuven.be>
+
 import numpy as np
 import argparse
 import pathlib
