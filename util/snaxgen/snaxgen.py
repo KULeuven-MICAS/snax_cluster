133675c075896d6651e5b1ca8fe0715f7f1c7f42
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2024-09-17 11:26:58 +0200

    py fmt

--- util/snaxgen/snaxgen.py
+++ util/snaxgen/snaxgen.py
@@ -382,7 +382,8 @@ def main():
             ):
                 raise ValueError(
                     "Missing gemmX configuration. \n"
-                    "Please set snax_gemmx_mesh_row, snax_gemmx_mesh_col, snax_gemmx_tile_size, with_pipeline"
+                    "Please set snax_gemmx_mesh_row, snax_gemmx_mesh_col, "
+                    "snax_gemmx_tile_size, with_pipeline"
                 )
             gen_chisel_file(
                 chisel_path=chisel_acc_path,
