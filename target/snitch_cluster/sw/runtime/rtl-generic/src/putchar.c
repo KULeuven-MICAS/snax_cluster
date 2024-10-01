ac3ab5204ce8efb9eabd72a03019eab2ec15cef1
Yunhao Deng <yunhao.deng@kuleuven.be>
2024-10-01 21:16:46 +0200

    New _putchar implementation for hostless execution

--- target/snitch_cluster/sw/runtime/rtl-generic/src/putchar.c
+++ target/snitch_cluster/sw/runtime/rtl-generic/src/putchar.c
@@ -3,4 +3,17 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Reuse same implementation as regular rtl runtime
-#include "../../rtl/src/putchar.c"
+// This is the putchar implementation for the testbench with fesvr
+// #include "../../rtl/src/putchar.c"
+
+#include "uart_snitch.h"
+
+static uint8_t uart_initialized = 0;
+
+void _putchar(char character) {
+    if (!uart_initialized) {
+        init_uart(32, 1);
+        uart_initialized = 1;
+    }
+    write_serial(character);
+}
