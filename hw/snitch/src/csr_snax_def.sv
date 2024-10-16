ff14337b26550f2b4e300fbcd0ac74729eb23fc1
rgantonio <ryan.antonio@esat.kuleuven.be>
2024-10-16 09:15:11 +0200

    hw: refactor location of observable csr register

--- hw/snitch/src/csr_snax_def.sv
+++ hw/snitch/src/csr_snax_def.sv
@@ -4,6 +4,5 @@ localparam logic [11:0] CSR_SNAX_BEGIN = 12'h3c0;
 localparam logic [11:0] CSR_SNAX_END = 12'h5ff;
 localparam logic [11:0] SNAX_CSR_BARRIER_EN = 12'h7c3;
 localparam logic [11:0] SNAX_CSR_BARRIER = 12'h7c4;
-localparam logic [11:0] SNAX_CSR_OBSERVE = 12'h7c5;
 endpackage
 // verilog_lint: waive-stop parameter-name-style
