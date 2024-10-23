a4aed130dcabf38e5f4358f79c2cfc5d892e55bd
IveanEx <yunhao.deng@kuleuven.be>
2024-10-23 21:03:23 +0200

    fmt+header

--- hw/spm_interface/src/spm_1p_adv_mem_wrapper.sv
+++ hw/spm_interface/src/spm_1p_adv_mem_wrapper.sv
@@ -10,57 +10,57 @@
 (* no_ungroup *)
 (* no_boundary_optimization *)
 module spm_1p_adv_mem_wrapper #(
-    parameter int unsigned NumWords   = 32'd1024, // Number of Words in data array
-    parameter int unsigned DataWidth  = 32'd128,  // Data signal width
-    parameter int unsigned ByteWidth  = 32'd8,    // Width of a data byte
-    parameter              SimInit      = "none",   // Simulation 
-    parameter bit          PrintSimCfg  = 1'b0,     // Print configuration
-    parameter type         impl_in_t  = logic,    // Type for implementation inputs
-    parameter type         impl_out_t = logic,
+    parameter int unsigned NumWords = 32'd1024,  // Number of Words in data array
+    parameter int unsigned DataWidth = 32'd128,  // Data signal width
+    parameter int unsigned ByteWidth = 32'd8,  // Width of a data byte
+    parameter string SimInit = "none",  // Simulation
+    parameter bit PrintSimCfg = 1'b0,  // Print configuration
+    parameter type impl_in_t = logic,  // Type for implementation inputs
+    parameter type impl_out_t = logic,
     // DEPENDENT PARAMETERS, DO NOT OVERWRITE!
-    parameter int unsigned AddrWidth  = (NumWords > 32'd1) ? $clog2(NumWords) : 32'd1,
-    parameter int unsigned BeWidth    = (DataWidth + ByteWidth - 32'd1) / ByteWidth, // ceil_div
-    parameter type         addr_t     = logic [AddrWidth-1:0],
-    parameter type         data_t     = logic [DataWidth-1:0],
-    parameter type         be_t       = logic [BeWidth-1:0]
+    parameter int unsigned AddrWidth = (NumWords > 32'd1) ? $clog2(NumWords) : 32'd1,
+    parameter int unsigned BeWidth = (DataWidth + ByteWidth - 32'd1) / ByteWidth,  // ceil_div
+    parameter type addr_t = logic [AddrWidth-1:0],
+    parameter type data_t = logic [DataWidth-1:0],
+    parameter type be_t = logic [BeWidth-1:0]
 
 ) (
-    input  logic                 clk_i,
-    input  logic                 rst_ni,
+    input  logic      clk_i,
+    input  logic      rst_ni,
     // implementation-related IO
-    input  impl_in_t             impl_i,
-    output impl_out_t            impl_o,
+    input  impl_in_t  impl_i,
+    output impl_out_t impl_o,
     // input ports
-    input  logic                 req_i,      // request
-    input  logic                 we_i,       // write enable
-    input  addr_t                addr_i,     // request address
-    input  data_t                wdata_i,    // write data
-    input  be_t                  be_i,       // write byte enable
+    input  logic      req_i,    // request
+    input  logic      we_i,     // write enable
+    input  addr_t     addr_i,   // request address
+    input  data_t     wdata_i,  // write data
+    input  be_t       be_i,     // write byte enable
     // output ports
-    output data_t                rdata_o     // read data
+    output data_t     rdata_o   // read data
 );
 
-    tc_sram_impl #(
-        .NumWords (NumWords),
-        .DataWidth(DataWidth),
-        .ByteWidth(ByteWidth),
-        .NumPorts (1),
-        .SimInit (SimInit),
-        .PrintSimCfg (PrintSimCfg),
-        .Latency  (1),
-        .impl_in_t (impl_in_t),
-        .impl_out_t (impl_out_t)
-    ) i_mem (
-        .clk_i(clk_i),
-        .rst_ni(rst_ni),
-        .impl_i (impl_i),
-        .impl_o (impl_o),
-        .req_i(req_i),
-        .we_i(we_i),
-        .addr_i(addr_i),
-        .wdata_i(wdata_i),
-        .be_i(be_i),
-        .rdata_o(rdata_o)
-    );
+  tc_sram_impl #(
+      .NumWords(NumWords),
+      .DataWidth(DataWidth),
+      .ByteWidth(ByteWidth),
+      .NumPorts(1),
+      .SimInit(SimInit),
+      .PrintSimCfg(PrintSimCfg),
+      .Latency(1),
+      .impl_in_t(impl_in_t),
+      .impl_out_t(impl_out_t)
+  ) i_mem (
+      .clk_i(clk_i),
+      .rst_ni(rst_ni),
+      .impl_i(impl_i),
+      .impl_o(impl_o),
+      .req_i(req_i),
+      .we_i(we_i),
+      .addr_i(addr_i),
+      .wdata_i(wdata_i),
+      .be_i(be_i),
+      .rdata_o(rdata_o)
+  );
 
 endmodule
