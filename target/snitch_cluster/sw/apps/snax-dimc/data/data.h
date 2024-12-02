6eaf3d6b62aba7d0572db0ab9bb259f1838bf15e
Ce Ma <cma@barnard3.esat.kuleuven.be>
2024-12-02 15:53:37 +0100

    for debug the memcpy issue

--- target/snitch_cluster/sw/apps/snax-dimc/data/data.h
+++ target/snitch_cluster/sw/apps/snax-dimc/data/data.h
@@ -1,7 +1,8 @@
-#include "conv_res_qkt.h"
-#include "conv_res_qkv.h"
+// #include "conv_res_qkt.h"
+// #include "conv_res_qkv.h"
 #include "kernel_stimulus.h"
 #include "mha_golden_result.h"
+#include "conv_golden_result.h"
 #include "Q1K1T_G.h"
 
 uint64_t Q1K1T_LENGTH = 512;
@@ -11,11 +12,11 @@ uint64_t Q_LENGTH = 4096;
 // here Q, K V and WQ, WK, WV should have the form of 1-D array
 // with a length of 4096, and elements of uint64_t
 #include "Q_content.h"
-#include "K_content.h"
-#include "V_content.h"
-#include "WQ_content.h"
-#include "WK_content.h"
-#include "WV_content.h"
+// #include "K_content.h"
+// #include "V_content.h"
+// #include "WQ_content.h"
+// #include "WK_content.h"
+// #include "WV_content.h"
 
 
 // DMA test
--- target/snitch_cluster/sw/apps/snax-dimc/src/snax-dimc.c
+++ target/snitch_cluster/sw/apps/snax-dimc/src/snax-dimc.c
@@ -2,285 +2,187 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-// this version of the code will only allocate 64KB of TCDM for the matrix Q/WQ/K/WK/V/WV
-// since the constraints of a limited TCDM (128-KB) and the existence of a memory stack
-
+#include "snrt.h"
 #include "snax-dimc-csr.h"
 #include "snax-dimc-lib.h"
-#include "snrt.h"
 #include "data.h"
 
 int main() {
 
-    printf("Starting the DIMC test\n");
+    printf("Starting the DIMC CONV test\n");
 
-    // set error value for checking
+    // Set err value for checking
     int err = 0;
 
-    // allocate 32+32+32KB in TCDM for activation and weight pair
-    uint64_t *activation_ptr, *weight_ptr, *output_ptr;
+    uint64_t kernel_length = 256;
+    uint64_t qkv_activation_length = Q_LENGTH;
+    uint64_t qkt_activation_length = Q1K1T_LENGTH;
 
-    activation_ptr = (uint64_t *)snrt_l1_next();
-    weight_ptr     = activation_ptr + Q_LENGTH;
-    output_ptr     = weight_ptr + Q_LENGTH;
+    uint8_t  *local_kernel;
+    uint64_t *local_activation_qkv, *local_activation_qkt, *local_final_res;
     
-    // allocate 4KB in TCDM for Q1K1T and final result
-    uint64_t *buffer_ptr = output_ptr + Q_LENGTH;
+    local_kernel = (uint8_t *)snrt_l1_next();
+    local_activation_qkv = (uint64_t *)(local_kernel + kernel_length);
+    local_activation_qkt = (uint64_t *)(local_activation_qkv + qkv_activation_length);
+    local_final_res = (uint64_t *)(local_activation_qkt + qkt_activation_length);
 
-    // alias for output_ptr, also holding Q
-    uint64_t *activation_ptr_i = output_ptr; 
+    if(snrt_is_dm_core()) {
+        printf("DMA core will be configured\n");
 
-    // stage 1:
-    // load WK, K, Q to TCDM
-
-    if (snrt_is_dm_core()) {
-        printf("DMA core is configured for K and WK\n");
-
-        // measure the start of cycle count for preloading data to TCDM
+        // This measures the start of cycle count
+        // for preloading the data to the L1 memory
         uint32_t start_dma_load = snrt_mcycle();
-
-        // initialize TCDM with matrix K by DMA
+        /**********************************************************************/
+        // initialize TCDM by DMA
+        /**********************************************************************/
         printf("INITIALIZING TCDM\n");
 
-        // read weight WK and ativation K from data.h
-        size_t vector_size = Q_LENGTH * sizeof(uint64_t);
+        // read matrix Q from data.h
+        int kernel_size = kernel_length * sizeof(uint8_t);
+        int activation_size_qkv = qkv_activation_length * sizeof(uint64_t);
+        int activation_size_qkt = qkt_activation_length * sizeof(uint64_t);
 
-        snrt_dma_start_1d(activation_ptr,   K,  vector_size);
-        snrt_dma_start_1d(activation_ptr_i, Q,  vector_size);
-        snrt_dma_start_1d(weight_ptr,       WK, vector_size);
-        
+        snrt_dma_start_1d(local_kernel,         kernel,  kernel_size);
+        snrt_dma_start_1d(local_activation_qkv, Q,       activation_size_qkv);
+        snrt_dma_start_1d(local_activation_qkt, Q1K1T_G, activation_size_qkt);
+
+        // Measure the end of the transfer process
         snrt_dma_wait_all();
-        
-        // measures the end of the DMA transfer process
+
         uint32_t end_dma_load = snrt_mcycle();
-        printf("DMA core exits after loading K and WK\n"); 
+        
+        printf("DMA core exits\n"); 
     }
 
-    /**************************************************************************/
-    // wait for the DMA to finish loading WK and K to TCDM
+    // Wait until DMA transfer is done
     snrt_cluster_hw_barrier();
-    /**************************************************************************/
-    // stage 2: all three regions are occupied with WK, K, and Q
-    // send WK from TCDM to DIMC
-    /**************************************************************************/
 
     if (snrt_is_compute_core()){
-        printf("COMPUTE CORE is configured\n");
+        printf("COMPUTE CORE\n");
 
+        /**********************************************************************/
         // configure the accelerator
-        printf("ENTERING MHA MODE\n");
-        
-        uint32_t busy = dimc_query_busy();
-        printf("%d: busy\n", busy);
-        printf("QUERYING BUSY SUCCEEDED\n");
+        /**********************************************************************/
+        printf("ENTERING CONV MODE\n");
+
+        dimc_query_busy();
+
+        // printf("QUERYING BUSY SUCCEEDED\n");
 
         configure_accelerator();
         
         printf("CONFIGURING ACCELERATOR SUCCEEDED\n");
+        /**********************************************************************/
+        // configure the accelerator
+        /**********************************************************************/
 
-        uint32_t read_zp_qkv = read_zp();
-        printf("%d: read_zp_qkv\n", read_zp_qkv);
-        printf("READING ZP SUCCEEDED\n");
-
-        // send WK
-        printf("CONFIGURING STREAMERS for WK\n");
-        dimc_set_streamer_dim_w(0, 0, 0, 0, 0, 0);
-        dimc_set_streamer_dim_r0(128, 1, 256, 0, 8, (uint32_t)(weight_ptr));
-        dimc_set_streamer_dim_r1(128, 1, 256, 0, 8, (uint32_t)(weight_ptr + 8));
-        dimc_set_streamer_dim_r2(128, 1, 256, 0, 8, (uint32_t)(weight_ptr + 16));
-        dimc_set_streamer_dim_r3(128, 1, 256, 0, 8, (uint32_t)(weight_ptr + 24));
-        printf("STREAMER CONFIGURED FOR WK\n");
+        // load convolution kernels
+        dimc_load_kernel();
         
-        // configure the accelerator to start MHA computation
-        dimc_start_mha();
+        // printf("STARTING LOADING KERNEL SUCCEEDED\n");
 
-        // start streamer data transfer
+        /**********************************************************************/
+        // configure the streamer
+        /**********************************************************************/
+
+        // LOAD KERNELS
+        printf("CONFIGURING STREAMERS for KERNEL\n");
+        dimc_set_streamer_dim_w(0, 0, 0, 0, 0, 0);
+        dimc_set_streamer_dim_r0(1, 1, 256, 0, 8, (uint32_t)(local_kernel));
+        dimc_set_streamer_dim_r1(1, 1, 256, 0, 8, (uint32_t)(local_kernel + 64));
+        dimc_set_streamer_dim_r2(1, 1, 256, 0, 8, (uint32_t)(local_kernel + 128));
+        dimc_set_streamer_dim_r3(1, 1, 256, 0, 8, (uint32_t)(local_kernel + 192));
+
         dimc_start_streamer();
 
+        while (dimc_is_streamer_busy()) {
+            // printf("STREAMER BUSY\n");
+        }
+
+        /**********************************************************************/
+        // configure the accelerator
+        /**********************************************************************/
+        dimc_load_act_conv();
         while (dimc_is_streamer_busy()) { }
-    }
-
-    /**************************************************************************/
-    // wait for streamer to finish sending WK to DIMC
-    snrt_cluster_hw_barrier();
-    /**************************************************************************/
-    // stage 3: weight_ptr is free, has K and Q in TCDM
-    // load WQ to TCDM;
-    // send K from TCDM to DIMC; kick start K1 generation;
-    /**************************************************************************/
-    
-    if (snrt_is_dm_core()) {
-        printf("DMA core is configured for WQ\n");
-
-        // read weight WQ from data.h
-        size_t vector_size = Q_LENGTH * sizeof(uint64_t);
-
-        snrt_dma_start_1d(weight_ptr, WQ, vector_size);
         
-        snrt_dma_wait_all();
-    }
+        // printf("STARTING LOADING ACTIVATION SUCCEEDED\n");
 
-    if (snrt_is_compute_core()){
-        // send K
-        printf("CONFIGURING STREAMERS for K\n");
-        dimc_set_streamer_dim_w(0, 0, 0, 0, 0, 0);
-        dimc_set_streamer_dim_r0(128, 1, 256, 0, 8, (uint32_t)(activation_ptr));
-        dimc_set_streamer_dim_r1(128, 1, 256, 0, 8, (uint32_t)(activation_ptr + 8));
-        dimc_set_streamer_dim_r2(128, 1, 256, 0, 8, (uint32_t)(activation_ptr + 16));
-        dimc_set_streamer_dim_r3(128, 1, 256, 0, 8, (uint32_t)(activation_ptr + 24));
-        printf("STREAMER CONFIGURED FOR K\n");
+        /**********************************************************************/
+        // configure the streamer
+        /**********************************************************************/
 
-        dimc_start_streamer();
-
-        while (dimc_is_streamer_busy()) { }
-    }
-
-    /**************************************************************************/
-    // wait for the DMA to finish loading WQ to TCDM and K1 generation
-    snrt_cluster_hw_barrier();
-    /**************************************************************************/
-    // stage 4: activation_ptr is free, has WQ and Q in TCDM
-    // streamer sends WQ to DIMC
-    /**************************************************************************/
-
-    if(snrt_is_compute_core()) {
-        // send WQ
-        printf("CONFIGURING STREAMERS for WQ\n");
-        dimc_set_streamer_dim_w(0, 0, 0, 0, 0, 0);
-        dimc_set_streamer_dim_r0(128, 1, 256, 0, 8, (uint32_t)(weight_ptr));
-        dimc_set_streamer_dim_r1(128, 1, 256, 0, 8, (uint32_t)(weight_ptr + 8));
-        dimc_set_streamer_dim_r2(128, 1, 256, 0, 8, (uint32_t)(weight_ptr + 16));
-        dimc_set_streamer_dim_r3(128, 1, 256, 0, 8, (uint32_t)(weight_ptr + 24));
-        printf("STREAMER CONFIGURED for WQ\n");
+        // LOAD ACTIVATION
+        printf("CONFIGURING STREAMERS for ACTIVATION\n");
+        // dimc_set_streamer_dim_w(0, 0, 0, 0, 0, 0);
+        // dimc_set_streamer_dim_r0(128, 1, 256, 0, 8, (uint32_t)(local_activation_qkv));
+        // dimc_set_streamer_dim_r1(128, 1, 256, 0, 8, (uint32_t)(local_activation_qkv + 8));
+        // dimc_set_streamer_dim_r2(128, 1, 256, 0, 8, (uint32_t)(local_activation_qkv + 16));
+        // dimc_set_streamer_dim_r3(128, 1, 256, 0, 8, (uint32_t)(local_activation_qkv + 24));
+        dimc_set_streamer_dim_w((64*16), 1, 64, 0, 8, (uint32_t)(local_final_res));
+        dimc_set_streamer_dim_r0(144, 1, 256, 0, 8, (uint32_t)(local_activation_qkv));
+        dimc_set_streamer_dim_r1(144, 1, 256, 0, 8, (uint32_t)(local_activation_qkv + 8));
+        dimc_set_streamer_dim_r2(144, 1, 256, 0, 8, (uint32_t)(local_activation_qkv + 16));
+        dimc_set_streamer_dim_r3(144, 1, 256, 0, 8, (uint32_t)(local_activation_qkv + 24));
+        // printf("STREAMER CONFIGURED for ACTIVATION\n");
 
         dimc_start_streamer();
-
-        while (dimc_is_streamer_busy()) { }
-    }
-
-    /**************************************************************************/
-    // wait for the streamer to finish sending WQ to DIMC
-    snrt_cluster_hw_barrier();
-    /**************************************************************************/
-    // stage 5: weight_ptr and actication_ptr are free, has Q in TCDM
-    // load WV and V to TCDM;
-    // streamer sends Q to DIMC & kick start Q1K1T generation;
-    /**************************************************************************/
-
-    if (snrt_is_dm_core()) {
-        printf("DMA core is configured for WQ\n");
-
-        // read weight WK and ativation K from data.h
-        size_t vector_size = Q_LENGTH * sizeof(uint64_t);
-
-        snrt_dma_start_1d(activation_ptr, V, vector_size);
-        snrt_dma_start_1d(weight_ptr, WV, vector_size);
         
-        snrt_dma_wait_all();
-    }
-    
-    if (snrt_is_compute_core()){
-        // send Q
-        printf("CONFIGURING STREAMERS for Q\n");
-        dimc_set_streamer_dim_w(64, 1, 64, 0, 8, (uint32_t)(buffer_ptr));
-        dimc_set_streamer_dim_r0(128, 1, 256, 0, 8, (uint32_t)(activation_ptr_i));
-        dimc_set_streamer_dim_r1(128, 1, 256, 0, 8, (uint32_t)(activation_ptr_i + 8));
-        dimc_set_streamer_dim_r2(128, 1, 256, 0, 8, (uint32_t)(activation_ptr_i + 16));
-        dimc_set_streamer_dim_r3(128, 1, 256, 0, 8, (uint32_t)(activation_ptr_i + 24));
-        printf("STREAMER CONFIGURED for Q\n");
+        /**********************************************************************/
+        // configure the accelerator
+        /**********************************************************************/
 
-        dimc_start_streamer();
+        dimc_start_conv();
 
-        while (dimc_is_streamer_busy()) { }
-    }
-
-    /**************************************************************************/
-    // wait for the streamer to finish receiving Q1K1T
-    snrt_cluster_hw_barrier();
-    /**************************************************************************/
-    // stage 6: activation_ptr_i is free, has V and WV in TCDM
-    // send V to DIMC
-    /**************************************************************************/
-
-    if (snrt_is_compute_core()){
-        // send V
-        printf("CONFIGURING STREAMERS for V\n");
-        dimc_set_streamer_dim_w(0, 0, 0, 0, 0, 0);
-        dimc_set_streamer_dim_r0(128, 1, 256, 0, 8, (uint32_t)(activation_ptr));
-        dimc_set_streamer_dim_r1(128, 1, 256, 0, 8, (uint32_t)(activation_ptr + 8));
-        dimc_set_streamer_dim_r2(128, 1, 256, 0, 8, (uint32_t)(activation_ptr + 16));
-        dimc_set_streamer_dim_r3(128, 1, 256, 0, 8, (uint32_t)(activation_ptr + 24));
-        printf("STREAMER CONFIGURED for V\n");
-
-        dimc_start_streamer();
-
-        while (dimc_is_streamer_busy()) { }
-    }
-
-    /**************************************************************************/
-    // wait for the streamer to finish sending V to DIMC
-    snrt_cluster_hw_barrier();
-    /**************************************************************************/
-    // stage 7: activation_ptr, activation_ptr_i are free, has WV in TCDM
-    // send WV to DIMC
-    /**************************************************************************/
-
-    if (snrt_is_compute_core()) {
-        // send WV
-        printf("CONFIGURING STREAMERS for WV\n");
-        dimc_set_streamer_dim_w(0, 0, 0, 0, 0, 0);
-        dimc_set_streamer_dim_r0(128, 1, 256, 0, 8, (uint32_t)(weight_ptr));
-        dimc_set_streamer_dim_r1(128, 1, 256, 0, 8, (uint32_t)(weight_ptr + 8));
-        dimc_set_streamer_dim_r2(128, 1, 256, 0, 8, (uint32_t)(weight_ptr + 16));
-        dimc_set_streamer_dim_r3(128, 1, 256, 0, 8, (uint32_t)(weight_ptr + 24));
-        printf("STREAMER CONFIGURED for WV\n");
-
-        dimc_start_streamer();
-
-        while (dimc_is_streamer_busy()) { }
-    }
-
-    /**************************************************************************/
-    // wait for the streamer to finish sending WV to DIMC and V1 generation
-    snrt_cluster_hw_barrier();
-    /**************************************************************************/
-    // stage 8: activation_ptr, activation_ptr_i, weight_ptr are free
-    // has Q1K1T in TCDM
-    // send Q1K1T to DIMC, saving final result in 
-    /**************************************************************************/
-
-    if (snrt_is_compute_core()) {
-        // send Q1K1T
-        printf("CONFIGURING STREAMERS for Q1K1T\n");
-        dimc_set_streamer_dim_w(64, 1, 64, 0, 8, (uint32_t)(output_ptr));
-        dimc_set_streamer_dim_r0(16, 1, 256, 0, 8, (uint32_t)(buffer_ptr));
-        dimc_set_streamer_dim_r1(16, 1, 256, 0, 8, (uint32_t)(buffer_ptr + 8));
-        dimc_set_streamer_dim_r2(16, 1, 256, 0, 8, (uint32_t)(buffer_ptr + 16));
-        dimc_set_streamer_dim_r3(16, 1, 256, 0, 8, (uint32_t)(buffer_ptr + 24));
-        printf("STREAMER CONFIGURED for Q1K1T\n");
-
-        dimc_start_streamer();
-
+        // Wait until the accelerator is done
         while (dimc_is_streamer_busy()) { }
 
-        printf("CHECK FINAL RESULT\n");
+        printf("GO CHECK FINAL RESULT\n");
+        
+        int mismatch = 0;
 
-        // check the final result
-        for (int i = 0; i < 512; ++i) {
-            uint64_t value = output_ptr[i];
+        // const int iterations = 8192 / 8;
+        for (int i = 0; i < (8192 / 8); i++) {
+            // Pointers to the current chunks
+            uint64_t* res_chunk = &local_final_res[i * 8];
+            uint8_t* gold_chunk = &gold_conv[i * 36];
 
-            uint64_t index = i * 8;
+            // Buffer to hold bits from local_final_res
+            uint8_t res_bits[64]; // 512 bits / 8 bits per byte = 64 bytes
 
-            // Split each uint64_t element into 8 uint8_t elements
-            for (int j = 0; j < 8; ++j) {
-                uint8_t tmp_res = (uint8_t)((value >> (j * 8)) & 0xFF);
-                // printf("%d ", tmp_res);
-                if(tmp_res != gold[index + j]) {
-                    printf("MISMATCH at %d, res:%d, gold:%d\n", (index + j), tmp_res, gold[index + j]);
+            // Convert local_final_res chunk to bytes
+            for (int j = 0; j < 8; j++) {
+                uint64_t val = res_chunk[j];
+                for (int k = 0; k < 8; ++k) {
+                    res_bits[j * 8 + k] = (val >> (k * 8)) & 0xFF; // Little-endian
+                }
+            }
+
+            // Buffer to hold bits from gold_conv with padding
+            
+            uint8_t gold_bits[64];
+            
+            for (int j = 0; j < 36; j++) {
+                gold_bits[j] = gold_chunk[j];
+            }
+
+            for (int j = 36; j < 64; j++) {
+                gold_bits[j] = 0;
+            }
+
+            // Compare the two byte arrays
+            bool match = true;
+            for (int j = 0; j < 64; j++) {
+                if (res_bits[j] != gold_bits[j]) {
+                    match = false;
+                    mismatch++;
+                    printf("mismatch\n");;
                     break;
                 }
             }
-            // printf("RESULTS MATCH WITH GOLDEN MODEL\n");
         }
     }
+
+
+    // printf("DIMC CONV test finished, %d\n", err);
+    return err;
 }
