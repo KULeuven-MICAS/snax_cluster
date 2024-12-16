4f24312494dfe13e54503dc1864e170b2fd71736
Josse Van Delm <josse.vandelm@gmail.com>
2024-12-16 18:06:25 +0100

    Update license header

--- util/conda/build.sh
+++ util/conda/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright 2024 KU Leuven.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
 
 docker create --name dummy ghcr.io/kuleuven-micas/snax:latest
 docker cp dummy:/opt ${PREFIX}/snax-utils
