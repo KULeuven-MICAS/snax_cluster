99b21ace267adc3d147f40d759ad5099dd1175be
Josse Van Delm <josse.vandelm@gmail.com>
2024-12-17 12:54:20 +0100

    Add spike-dasm to conda package

--- util/conda/build.sh
+++ util/conda/build.sh
@@ -5,3 +5,6 @@
 
 docker create --name dummy ghcr.io/kuleuven-micas/snax:latest
 docker cp dummy:/opt ${PREFIX}/snax-utils
+mkdir -p ${PREFIX}/bin
+docker cp dummy:/bin/spike-dasm ${PREFIX}/bin/spike-dasm
+docker rm dummy
