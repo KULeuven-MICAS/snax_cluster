131e9ab15edf39cd7f877fc0a2cedddd30f4ba15
Josse Van Delm <josse.vandelm@gmail.com>
2025-01-21 15:25:11 +0100

    Fix cp being not docker cp

--- util/conda/build.sh
+++ util/conda/build.sh
@@ -3,16 +3,6 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
  
-
-clean_and_fetch_git() {
-    cd ..
-    rm -rf snax_cluster
-    git clone --recursive https://github.com/kuleuven-micas/snax_cluster
-    cd snax_cluster
-    git checkout 86e2a519805b255377ed379d08686248379a24d5
-    return 0
-}
-
 # Function to build multiple snitch_cluster.vlt instances
 build_snax_verilator() {
     local config_file="$1"
@@ -46,15 +36,24 @@ build_snax_verilator() {
     
     echo "Build completed for '$config_file' successfully"
 
-    cp --parents target/snitch_cluster/bin/snitch_cluster.vlt ${target_dir}/bin/snitch_cluster.vlt && \
-    cp --parents sw/snRuntime ${target_dir}/sw/snRuntime && \
-    cp --parents target/snitch_cluster/sw/runtime/rtl ${target_dir}/target/snitch_cluster/sw/runtime/rtl && \
-    cp --parents target/snitch_cluster/sw/runtime/rtl-generic ${target_dir}/target/snitch_cluster/sw/runtime/rtl-generic && \
-    cp --parents target/snitch_cluster/sw/runtime/common ${target_dir}/target/snitch_cluster/sw/runtime/common && \
-    cp --parents target/snitch_cluster/sw/snax/ ${target_dir}/target/snitch_cluster/sw/snax && \
-    cp --parents sw/math/ ${target_dir}/sw/math/ && \
-    cp --parents sw/deps/riscv-opcodes ${target_dir}/sw/deps/riscv-opcodes && \
-    cp --parents sw/deps/printf ${target_dir}/sw/deps/printf
+    mkdir -p ${target_dir}/bin
+    cp target/snitch_cluster/bin/snitch_cluster.vlt ${target_dir}/bin/snitch_cluster.vlt
+    mkdir -p ${target_dir}/sw/snRuntime/
+    cp -R sw/snRuntime/ ${target_dir}/sw/snRuntime/
+    mkdir -p ${target_dir}/target/snitch_cluster/sw/runtime/rtl/
+    cp -R target/snitch_cluster/sw/runtime/rtl/ ${target_dir}/target/snitch_cluster/sw/runtime/rtl/
+    mkdir -p ${target_dir}/target/snitch_cluster/sw/runtime/rtl-generic/
+    cp -R target/snitch_cluster/sw/runtime/rtl-generic/ ${target_dir}/target/snitch_cluster/sw/runtime/rtl-generic/
+    mkdir -p ${target_dir}/target/snitch_cluster/sw/runtime/common
+    cp -R target/snitch_cluster/sw/runtime/common/ ${target_dir}/target/snitch_cluster/sw/runtime/common/
+    mkdir -p ${target_dir}/target/snitch_cluster/sw/snax
+    cp -R target/snitch_cluster/sw/snax/ ${target_dir}/target/snitch_cluster/sw/snax
+    mkdir -p ${target_dir}/sw/math/
+    cp -R sw/math/ ${target_dir}/sw/math/
+    mkdir -p ${target_dir}/sw/deps/riscv-opcodes
+    cp -R sw/deps/riscv-opcodes/ ${target_dir}/sw/deps/riscv-opcodes
+    mkdir -p ${target_dir}/sw/deps/printf
+    cp -R sw/deps/printf/ ${target_dir}/sw/deps/printf
 
     echo "Successfully installed in '$target_dir'"
 
