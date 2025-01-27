69eeec1248eb3109e2fa6a0707bcf0049b6b2d6a
Josse Van Delm <josse.vandelm@gmail.com>
2025-01-27 13:29:49 +0100

    Add stupid license header

--- util/conda/build-dev.sh
+++ util/conda/build-dev.sh
@@ -1,1 +1,6 @@
-# This file is intentionally empty
+#!/bin/bash
+# Copyright 2025 KU Leuven.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+ 
+# This file is intentionally left empty
