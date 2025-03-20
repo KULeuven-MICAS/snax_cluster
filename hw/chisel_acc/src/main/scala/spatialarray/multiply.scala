8b8ad3502612eae8e1814aededaaed64d82c46c7
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2025-03-20 20:10:21 +0100

    configurable su init

---
+++ hw/chisel_acc/src/main/scala/spatialarray/multiply.scala
@@ -0,0 +1,42 @@
+package snax_acc.spatialarray
+
+import chisel3._
+
+class MultiplierIO[T <: Data with Num[T]](
+    inputAType: T,
+    inputBType: T,
+    mulType: T
+) extends Bundle {
+  val in_a = Input(inputAType)
+  val in_b = Input(inputBType)
+  val out_c = Output(mulType)
+}
+
+class Multiplier[T <: Data with Num[T]](
+    inputAType: T,
+    inputBType: T,
+    mulType: T
+) extends Module {
+  val io = IO(new MultiplierIO(inputAType, inputBType, mulType))
+  io.out_c := io.in_a * io.in_b
+}
+
+object MultiplierEmitterUInt extends App {
+  println(
+    getVerilogString(
+      new Multiplier(UInt(8.W), UInt(4.W), UInt(16.W))
+    )
+  )
+}
+
+object MultiplierEmitterSInt extends App {
+  println(
+    getVerilogString(
+      new Multiplier(SInt(8.W), SInt(4.W), SInt(16.W))
+    )
+  )
+  emitVerilog(
+    new Multiplier(SInt(8.W), SInt(4.W), SInt(16.W)),
+    Array("--target-dir", "generated/SpacialArray")
+  )
+}
