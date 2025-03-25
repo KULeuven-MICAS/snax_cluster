ee16f595d54d69bd86de2d3d641b2f359dfc361c
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2025-03-25 14:06:04 +0100

    add fp-fp test

--- hw/chisel_acc/src/main/scala/spatial_array/fsm.scala
+++ hw/chisel_acc/src/main/scala/spatial_array/fsm.scala
@@ -1,1 +1,67 @@
+package snax_acc.spatial_array
 
+import chisel3._
+import chisel3.util._
+
+class BasicCounter(width: Int, hasCeil: Boolean = true)
+    extends Module
+    with RequireAsyncReset {
+  val io = IO(new Bundle {
+    val tick = Input(Bool())
+    val reset = Input(Bool())
+    val ceil = Input(UInt(width.W))
+
+    val value = Output(UInt(width.W))
+    val lastVal = Output(Bool())
+  })
+  // 32.W should be enough to count any loops
+  val nextValue = Wire(UInt(width.W))
+  val value = RegNext(nextValue, 0.U)
+  nextValue := {
+    if (hasCeil) {
+      Mux(
+        io.reset,
+        0.U,
+        Mux(io.tick, Mux(value < io.ceil - 1.U, value + 1.U, 0.U), value)
+      )
+    } else {
+      Mux(io.reset, 0.U, Mux(io.tick, value + 1.U, value))
+    }
+  }
+
+  io.value := value
+  io.lastVal := {
+    (if (hasCeil) (value === io.ceil - 1.U) else (value.andR)) && io.tick
+  }
+}
+
+class NestCounter(width: Int, loopNum: Int)
+    extends Module
+    with RequireAsyncReset {
+  val io = IO(new Bundle {
+    val tick = Input(Bool())
+    val reset = Input(Bool())
+    val ceil = Input(Vec(loopNum, UInt(width.W)))
+
+    val value = Output(Vec(loopNum, UInt(width.W)))
+    val lastVal = Output(Vec(loopNum, Bool()))
+  })
+
+  val counter = Seq.fill(loopNum)(Module(new BasicCounter(width, true)))
+
+  counter.zipWithIndex.map { case (c, i) =>
+    c.io.reset := io.reset
+    c.io.ceil := io.ceil(i)
+  }
+
+  // Connect the tick signal to the first counter
+  counter(0).io.tick := io.tick
+
+  // Connect the tick signal to the rest of the counters
+  counter.zip(counter.tail).map { case (c1, c2) =>
+    c2.io.tick := c1.io.lastVal && io.tick
+  }
+
+  io.value := counter.map(_.io.value)
+  io.lastVal := counter.map(_.io.lastVal)
+}
