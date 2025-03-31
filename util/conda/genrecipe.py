4667189083be48255e3bdfe879f6eab9289dcee4
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2025-03-31 10:12:41 +0200

    further remove reshuffler

--- util/conda/genrecipe.py
+++ util/conda/genrecipe.py
@@ -29,11 +29,11 @@ PREAMBLE = r"""
 
 context:
   git_repo_url: "https://github.com/kuleuven-micas/snax_cluster"
-  version: 0.2.12.micro_v4
+  version: 0.2.12.micro_v5
 
 source:
   git: https://github.com/KULeuven-MICAS/snax_cluster
-  tag: v0.2.12.micro_v4
+  tag: v0.2.12.micro_v5
 
 about:
   homepage: https://github.com/KULeuven-MICAS/snax_cluster
