69ea69e3ea86476f62ffda4c025d681c77903207
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2025-04-05 20:26:34 +0200

    fifo 0

--- util/conda/genrecipe.py
+++ util/conda/genrecipe.py
@@ -29,11 +29,11 @@ PREAMBLE = r"""
 
 context:
   git_repo_url: "https://github.com/kuleuven-micas/snax_cluster"
-  version: 0.2.12.micro_v10
+  version: 0.2.12.micro_v10_fifo0
 
 source:
   git: https://github.com/KULeuven-MICAS/snax_cluster
-  tag: v0.2.12.micro_v10
+  tag: v0.2.12.micro_v10_fifo0
 
 about:
   homepage: https://github.com/KULeuven-MICAS/snax_cluster
