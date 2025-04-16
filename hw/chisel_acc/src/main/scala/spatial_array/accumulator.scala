c9c191259004943b0e668b3a4092352f459654ae
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2025-04-16 13:56:52 +0200

    reduce serial input/output bw to real and refatcor accmulator

--- hw/chisel_acc/src/main/scala/spatial_array/accumulator.scala
+++ hw/chisel_acc/src/main/scala/spatial_array/accumulator.scala
@@ -3,6 +3,44 @@ package snax_acc.spatial_array
 import chisel3._
 import chisel3.util._
 
+class AccumulatorBlock(
+  val opType:          Int,
+  val inputElemWidth:  Int,
+  val outputElemWidth: Int,
+) extends Module with RequireAsyncReset{
+  val io = IO(new Bundle {
+    val in1 = Input(UInt(inputElemWidth.W))
+    val in2 = Input(UInt(inputElemWidth.W))
+    val accAddExtIn = Input(Bool())
+    val enable = Input(Bool())
+    val accClear = Input(Bool())
+    val out = Output(UInt(outputElemWidth.W))
+  }
+  )
+
+  require(opType == OpType.UIntUIntOp || opType == OpType.SIntSIntOp)
+  require(
+    inputElemWidth > 0 && outputElemWidth > 0,
+    "Element widths must be greater than 0"
+  )
+
+  val accumulatorReg = RegInit(0.U(outputElemWidth.W))
+  val adder = Module(
+    new Adder(opType, inputElemWidth, inputElemWidth, outputElemWidth)
+  ).io
+  
+  adder.in_a := io.in1
+  adder.in_b := Mux(io.accAddExtIn, io.in2, accumulatorReg)
+
+  val nextAcc = Wire(UInt(outputElemWidth.W))
+  nextAcc := Mux(io.accClear, 0.U, Mux(io.enable, adder.out_c, accumulatorReg))
+  
+  val accUpdate = io.enable || io.accClear
+  accumulatorReg := Mux(accUpdate, nextAcc, accumulatorReg)
+
+  io.out := accumulatorReg
+}
+
 // accumulator
 class Accumulator(
   val opType:          Int,
@@ -26,59 +64,21 @@ class Accumulator(
     "Element widths and number of elements must be greater than 0"
   )
 
-  val accumulatorReg = RegInit(
-    VecInit(Seq.fill(numElements)(0.U(outputElemWidth.W)))
-  )
-
-  // Create the adders once outside the when block
-  val adders = Seq.fill(numElements)(
-    Module(
-      new Adder(opType, inputElemWidth, inputElemWidth, outputElemWidth)
-    ).io
-  )
-
-  // if not enabled, data gated
-  when(io.accClear) {
-    accumulatorReg := VecInit(Seq.fill(numElements)(0.U(outputElemWidth.W)))
-    adders.zipWithIndex.foreach { case (adder, i) =>
-      adder.in_a := io.in1.bits(i)
-      adder.in_b := io.in2.bits(i)
-    }
+  val accumulater_blocks = Seq.fill(numElements) {
+    Module(new AccumulatorBlock(opType, inputElemWidth, outputElemWidth))
   }
-    .elsewhen(io.enable) {
-      when(io.in1.fire && io.in2.fire && io.accAddExtIn) {
-        accumulatorReg := adders.zip(io.in1.bits.zip(io.in2.bits)).map { case (adder, (a, b)) =>
-          adder.in_a := a
-          adder.in_b := b
-          adder.out_c
-        }
-      }.elsewhen(io.in1.fire && !io.accAddExtIn) {
-        accumulatorReg := adders.zip(io.in1.bits.zip(accumulatorReg)).map { case (adder, (a, acc)) =>
-          adder.in_a := a
-          adder.in_b := acc
-          adder.out_c
-        }
-      }.otherwise {
-        // If not firing, keep the accumulator value
-        accumulatorReg := accumulatorReg
-        // adder inputs are not used, so set them to 0
-        adders.zipWithIndex.foreach { case (adder, i) =>
-          adder.in_a := io.in1.bits(i)
-          adder.in_b := io.in2.bits(i)
-        }
-      }
-    }
-    .otherwise {
-      // If not firing, keep the accumulator value
-      accumulatorReg := accumulatorReg
-      // adder inputs are not used, so set them to 0
-      adders.zipWithIndex.foreach { case (adder, i) =>
-        adder.in_a := io.in1.bits(i)
-        adder.in_b := io.in2.bits(i)
-      }
-    }
 
-  val inputDataFire  = (RegNext(io.in1.fire && !io.accAddExtIn) || RegNext(io.in1.fire && io.in2.fire && io.accAddExtIn)) && io.enable
+  val accUpdate = (io.in1.fire && io.enable && (!io.accAddExtIn || (io.in2.fire && io.accAddExtIn)))
+
+  for (i <- 0 until numElements) {
+    accumulater_blocks(i).io.in1 := io.in1.bits(i)
+    accumulater_blocks(i).io.in2 := io.in2.bits(i)
+    accumulater_blocks(i).io.accAddExtIn := io.accAddExtIn
+    accumulater_blocks(i).io.enable := accUpdate
+    accumulater_blocks(i).io.accClear := io.accClear
+  }
+
+  val inputDataFire  = RegNext(accUpdate)
   val keepOutput     = RegInit(false.B)
   val keepOutputNext = io.out.valid && !io.out.ready
   keepOutput := keepOutputNext
@@ -87,13 +87,17 @@ class Accumulator(
   io.in1.ready := (!keepOutput) && (!keepOutputNext)
   io.in2.ready := (!keepOutput) && (!keepOutputNext)
 
-  io.out.bits  := accumulatorReg
+  io.out.bits  := accumulater_blocks.map(_.io.out)
   io.out.valid := inputDataFire || keepOutput
 }
 
 object AccumulatorEmitterUInt extends App {
-  emitVerilog(
-    new Accumulator(OpType.UIntUIntOp, 8, 16, 8),
-    Array("--target-dir", "generated/SpatialArray")
+  _root_.circt.stage.ChiselStage.emitSystemVerilogFile(
+    new Accumulator(OpType.UIntUIntOp, 8, 16, 4096),
+    Array("--target-dir", "generated/SpatialArray"),
+    Array(
+      "--split-verilog",
+      s"-o=generated/SpatialArray/Accumulator"
+    )
   )
 }
--- hw/chisel_acc/src/main/scala/spatial_array/arrayTopGen.scala
+++ hw/chisel_acc/src/main/scala/spatial_array/arrayTopGen.scala
@@ -1,12 +1,111 @@
 package snax_acc.spatial_array
 
-import chisel3._
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import sys.process._
 
 object ArrayTopGen {
   def main(args: Array[String]): Unit = {
+  // Current time with default format
+  var currentTime = LocalDateTime.now()
+  println(s"Current time (default): $currentTime")
+
+  // Formatted output (e.g., "2024-01-23 14:30:15")
+  var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+  println(s"Formatted time: ${currentTime.format(formatter)}")
+
+    // 16x8x8, 1x32x32 ISSCC reproduce
+    // generation time: ~1sec
+    // val params = SpatialArrayParam(
+    //     opType = Seq(OpType.SIntSIntOp),
+    //     macNum = Seq(1024),
+    //     inputAElemWidth = Seq(8),
+    //     inputBElemWidth = Seq(8),
+    //     inputCElemWidth = Seq(32),
+    //     mulElemWidth = Seq(16),
+    //     outElemWidth = Seq(32),
+    //     inputAWidth = 1024,
+    //     inputBWidth = 8192,
+    //     arrayInputCWidth = 4096,
+    //     arrayOutputDWidth = 4096,
+    //     arrayDim = Seq(Seq(Seq(16, 8, 8), Seq(1, 32, 32)))
+    // )
+    // val tag = "ISSCC"
+
+    // ----------------------------------------------
+    // scalability test
+    // ----------------------------------------------
+    // 256x256 TPU like reproduce
+    // val params = SpatialArrayParam(
+    //     opType = Seq(OpType.SIntSIntOp),
+    //     macNum = Seq(65536),
+    //     inputAElemWidth = Seq(8),
+    //     inputBElemWidth = Seq(8),
+    //     inputCElemWidth = Seq(32),
+    //     mulElemWidth = Seq(16),
+    //     outElemWidth = Seq(32),
+    //     inputAWidth = 2048,
+    //     inputBWidth = 2048,
+    //     arrayInputCWidth = 2097152,
+    //     arrayOutputDWidth = 2097152,
+    //     arrayDim = Seq(Seq(Seq(256, 1, 256)))
+    // )
+
+    // // 128x128 TPU like reproduce
+    // val params = SpatialArrayParam(
+    //     opType = Seq(OpType.SIntSIntOp),
+    //     macNum = Seq(16384),
+    //     inputAElemWidth = Seq(8),
+    //     inputBElemWidth = Seq(8),
+    //     inputCElemWidth = Seq(32),
+    //     mulElemWidth = Seq(16),
+    //     outElemWidth = Seq(32),
+    //     inputAWidth = 1024,
+    //     inputBWidth = 1024,
+    //     arrayInputCWidth = 524288,
+    //     arrayOutputDWidth = 524288,
+    //     arrayDim = Seq(Seq(Seq(128, 1, 128)))
+    // )
+
+    // 64x128 TPU like reproduce
+    // generation time: ~40mins
+    // val params = SpatialArrayParam(
+    //     opType = Seq(OpType.SIntSIntOp),
+    //     macNum = Seq(8192),
+    //     inputAElemWidth = Seq(8),
+    //     inputBElemWidth = Seq(8),
+    //     inputCElemWidth = Seq(32),
+    //     mulElemWidth = Seq(16),
+    //     outElemWidth = Seq(32),
+    //     inputAWidth = 512,
+    //     inputBWidth = 1024,
+    //     arrayInputCWidth = 262144,
+    //     arrayOutputDWidth = 262144,
+    //     arrayDim = Seq(Seq(Seq(64, 1, 128)))
+    // )
+
+    // 64x64 TPU like reproduce
+    // generation time: ~4mins
+    // val params = SpatialArrayParam(
+    //     opType = Seq(OpType.SIntSIntOp),
+    //     macNum = Seq(4096),
+    //     inputAElemWidth = Seq(8),
+    //     inputBElemWidth = Seq(8),
+    //     inputCElemWidth = Seq(32),
+    //     mulElemWidth = Seq(16),
+    //     outElemWidth = Seq(32),
+    //     inputAWidth = 512,
+    //     inputBWidth = 512,
+    //     arrayInputCWidth = 131072,
+    //     arrayOutputDWidth = 131072,
+    //     arrayDim = Seq(Seq(Seq(64, 1, 64)))
+    // )
+
+    // Bitwave like reproduce
+    // 7 dfs
     val params = SpatialArrayParam(
         opType = Seq(OpType.SIntSIntOp),
-        macNum = Seq(1024),
+        macNum = Seq(4096),
         inputAElemWidth = Seq(8),
         inputBElemWidth = Seq(8),
         inputCElemWidth = Seq(32),
@@ -14,19 +113,36 @@ object ArrayTopGen {
         outElemWidth = Seq(32),
         inputAWidth = 1024,
         inputBWidth = 8192,
-        arrayInputCWidth = 4096,
-        arrayOutputDWidth = 4096,
-        arrayDim = Seq(Seq(Seq(16, 8, 8), Seq(1, 32, 32)))
+        arrayInputCWidth = 16384,
+        arrayOutputDWidth = 16384,
+        arrayDim = Seq(Seq(Seq(16, 8, 32), Seq(8, 16, 32), Seq(4, 32, 32), Seq(1, 8, 128), Seq(1, 16, 64), Seq(1, 32, 32), Seq(2, 1, 64)))
+    )
+    val tag = "BitWave"
+
+    println("arayDim: " + params.arrayDim.toString())
+
+    // generate verilog file
+    _root_.circt.stage.ChiselStage.emitSystemVerilogFile(
+      new ArrayTop(params),
+      Array(
+        "--target-dir", "generated/SpatialArray",
+      ),
+      // Array(
+      //   "--split-verilog",
+      //   s"-o=generated/SpatialArray/ArrayTop_${tag}"
+      // )
     )
 
-    emitVerilog(
-        new ArrayTop(params),
-        Array("--target-dir", "generated/SpatialArray")
-    )
+    s"cp generated/SpatialArray/ArrayTop.sv generated/SpatialArray/ArrayTop_${tag}.sv".!
+
+    currentTime = LocalDateTime.now()
+    println(s"Current time (default): $currentTime")
+    formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+    println(s"Formatted time: ${currentTime.format(formatter)}")
 
     // generate sv wrapper file
     var macro_template = ""
-    val macro_dir      = "./src/snax_streamer_gemmX_shell_wrapper.sv"
+    val macro_dir      = "./src/snax_opengemm_shell_wrapper.sv"
     val header         = s"""// Copyright 2025 KU Leuven.
 // Solderpad Hardware License, Version 0.51, see LICENSE for details.
 // SPDX-License-Identifier: SHL-0.51
@@ -45,7 +161,7 @@ object ArrayTopGen {
     val DataWidthD   = params.outputDSerialDataWidth
 
     macro_template = header + s"""
-module snax_streamer_gemmX_shell_wrapper #(
+module snax_opengemm_shell_wrapper #(
     // Custom parameters. As much as possible,
     // these parameters should not be taken from outside
     parameter int unsigned RegRWCount   = ${params.csrNum},
@@ -140,7 +256,6 @@ endmodule
     )
 
     println(s"Generated macro file: $macro_dir")
-
 
   }
 }
