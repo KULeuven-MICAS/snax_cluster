c7860cc2f9e5b153f439a34f39a442868b44715e
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2025-04-20 19:46:35 +0200

    add ci

--- util/snaxgen/snaxgen.py
+++ util/snaxgen/snaxgen.py
@@ -585,7 +585,11 @@ def main():
             snax_opengemm_cfg = acc_cfgs[i]
             gen_chisel_file(
                 chisel_path=chisel_acc_path,
-                chisel_param="snax_acc.spatial_array.ArrayTopGen" + " --openGeMMCfg " + hjson.dumpsJSON(obj=snax_opengemm_cfg, separators=(",", ":")).replace(" ", ""),
+                chisel_param="snax_acc.spatial_array.ArrayTopGen"
+                + " --openGeMMCfg "
+                + hjson.dumpsJSON(obj=snax_opengemm_cfg, separators=(",", ":")).replace(
+                    " ", ""
+                ),
                 gen_path=" --hw-target-dir " + rtl_target_path,
             )
         elif acc_cfgs[i]["snax_acc_name"] == "snax_data_reshuffler":
