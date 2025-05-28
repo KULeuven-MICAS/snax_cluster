00484e1249152cc3fde877443a11b662e64ce425
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2025-05-29 00:04:21 +0200

    add fp-int mul test modify from fp-uint test

--- hw/chisel_acc/src/test/scala/spatial_array/fp_mul_int_Test.scala
+++ hw/chisel_acc/src/test/scala/spatial_array/fp_mul_int_Test.scala
@@ -10,7 +10,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 trait FPMULIntTestUtils extends fpUtils {
 
-  def test_fp_mul_int(dut: FPMULInt, test_id: Int, A: Float, B: Int, C: Float) = {
+  def test_fp_mul_int(dut: FPMULInt, test_id: Int, A: Float, B: Int, C: Float, Bwidth: Int) = {
     val A_fp16 = uintToFloat(
       fp16.expWidth,
       fp16.sigWidth,
@@ -24,12 +24,12 @@ trait FPMULIntTestUtils extends fpUtils {
     val gold_O = A_fp16 * B // Expected result
 
     val stimulus_a_i = floatToUInt(fp16.expWidth, fp16.sigWidth, A)      // Convert to 16-bit representation
-    val stimulus_b_i = B                                                 // Direct 4-bit integer
+    val stimulus_b_i = (BigInt(B) & ((BigInt(1) << Bwidth) - 1)).U // B is an integer, int2uint conversion needed for sending it to the DUT
     val stimulus_c_i = floatToUInt(fp32.expWidth, fp32.sigWidth, C)      // Convert to 32-bit representation
     val expected_o   = floatToUInt(fp32.expWidth, fp32.sigWidth, gold_O) // Expected output as UInt
 
     dut.io.operand_a_i.poke(stimulus_a_i.U)
-    dut.io.operand_b_i.poke(stimulus_b_i.U)
+    dut.io.operand_b_i.poke(stimulus_b_i)
     dut.io.operand_c_i.poke(stimulus_c_i.U)
 
     dut.clock.step()
@@ -75,25 +75,25 @@ class FPMULInt8Test extends AnyFlatSpec with ChiselScalatestTester with fpUtils 
       var B = 1    // uint8
       var C = 1.0f // float32
 
-      test_fp_mul_int(dut, 1, A, B, C)
+      test_fp_mul_int(dut, 1, A, B, C, 8)
 
       A = 0.5f
       B = 2
       C = 2.3f
 
-      test_fp_mul_int(dut, 2, A, B, C)
+      test_fp_mul_int(dut, 2, A, B, C, 8)
 
       A = 0.005f
       B = 7
       C = 100f
 
-      test_fp_mul_int(dut, 3, A, B, C)
+      test_fp_mul_int(dut, 3, A, B, C, 8)
 
       A = 0.005f
       B = 0
       C = 10f
 
-      test_fp_mul_int(dut, 4, A, B, C)
+      test_fp_mul_int(dut, 4, A, B, C, 8)
 
       new Random()
 
@@ -105,13 +105,13 @@ class FPMULInt8Test extends AnyFlatSpec with ChiselScalatestTester with fpUtils 
       val testCases = Seq.fill(test_num)(
         (
           (Random.nextFloat() * 20 - 10), // A: Random float between -10 and 10
-          Random.nextInt(255),            // B: Random int between 0 and 255
+          Random.nextInt(255) - 128,            // B: Random int between -128 and 127
           (Random.nextFloat() * 20 - 10)
         )                                 // C: Random float between -10 and 10
       )
 
       testCases.zipWithIndex.foreach { case ((a, b, c), index) =>
-        test_fp_mul_int(dut, index + 1, a, b, c)
+        test_fp_mul_int(dut, index + 1, a, b, c, 8)
       }
 
     }
@@ -137,25 +137,25 @@ class FPMULInt4Test extends AnyFlatSpec with ChiselScalatestTester with fpUtils 
       var B = 1    // uint4
       var C = 1.0f // float32
 
-      test_fp_mul_int(dut, 1, A, B, C)
+      test_fp_mul_int(dut, 1, A, B, C, 4)
 
       A = 0.5f
       B = 2
       C = 2.3f
 
-      test_fp_mul_int(dut, 2, A, B, C)
+      test_fp_mul_int(dut, 2, A, B, C, 4)
 
       A = 0.005f
       B = 7
       C = 100f
 
-      test_fp_mul_int(dut, 3, A, B, C)
+      test_fp_mul_int(dut, 3, A, B, C, 4)
 
       A = 0.005f
       B = 0
       C = 10f
 
-      test_fp_mul_int(dut, 4, A, B, C)
+      test_fp_mul_int(dut, 4, A, B, C, 4)
 
       new Random()
 
@@ -167,13 +167,13 @@ class FPMULInt4Test extends AnyFlatSpec with ChiselScalatestTester with fpUtils 
       val testCases = Seq.fill(test_num)(
         (
           (Random.nextFloat() * 20 - 10), // A: Random float between -10 and 10
-          Random.nextInt(15),             // B: Random int between 0 and 15
+          Random.nextInt(15) - 8,             // B: Random int between -8 and 7
           (Random.nextFloat() * 20 - 10)
         )                                 // C: Random float between -10 and 10
       )
 
       testCases.zipWithIndex.foreach { case ((a, b, c), index) =>
-        test_fp_mul_int(dut, index + 1, a, b, c)
+        test_fp_mul_int(dut, index + 1, a, b, c, 4)
       }
 
     }
@@ -197,25 +197,25 @@ class FPMULInt3Test extends AnyFlatSpec with ChiselScalatestTester with fpUtils 
       var B = 1    // uint4
       var C = 1.0f // float32
 
-      test_fp_mul_int(dut, 1, A, B, C)
+      test_fp_mul_int(dut, 1, A, B, C, 3)
 
       A = 0.5f
       B = 2
       C = 2.3f
 
-      test_fp_mul_int(dut, 2, A, B, C)
+      test_fp_mul_int(dut, 2, A, B, C, 3)
 
       A = 0.005f
       B = 3
       C = 100f
 
-      test_fp_mul_int(dut, 3, A, B, C)
+      test_fp_mul_int(dut, 3, A, B, C, 3)
 
       A = 0.005f
       B = 0
       C = 10f
 
-      test_fp_mul_int(dut, 4, A, B, C)
+      test_fp_mul_int(dut, 4, A, B, C, 3)
 
       val random = new Random()
 
@@ -227,13 +227,13 @@ class FPMULInt3Test extends AnyFlatSpec with ChiselScalatestTester with fpUtils 
       val testCases = Seq.fill(test_num)(
         (
           (random.nextFloat() * 20 - 10), // A: Random float between -10 and 10
-          random.nextInt(7),              // B: Random int between 0 and 15
+          random.nextInt(7) - 3,              // B: Random int between -3 and 3
           (random.nextFloat() * 20 - 10)
         )                                 // C: Random float between -10 and 10
       )
 
       testCases.zipWithIndex.foreach { case ((a, b, c), index) =>
-        test_fp_mul_int(dut, index + 1, a, b, c)
+        test_fp_mul_int(dut, index + 1, a, b, c, 3)
       }
 
     }
@@ -256,25 +256,25 @@ class FPMULInt2Test extends AnyFlatSpec with ChiselScalatestTester with fpUtils 
       var B = 1    // uint4
       var C = 1.0f // float32
 
-      test_fp_mul_int(dut, 1, A, B, C)
+      test_fp_mul_int(dut, 1, A, B, C, 2)
 
       A = 0.5f
       B = 1
       C = 2.3f
 
-      test_fp_mul_int(dut, 2, A, B, C)
+      test_fp_mul_int(dut, 2, A, B, C, 2)
 
       A = 0.015f
       B = 0
       C = 100f
 
-      test_fp_mul_int(dut, 3, A, B, C)
+      test_fp_mul_int(dut, 3, A, B, C, 2)
 
       A = 0.005f
       B = 0
       C = 10f
 
-      test_fp_mul_int(dut, 4, A, B, C)
+      test_fp_mul_int(dut, 4, A, B, C, 2)
 
       val random = new Random()
 
@@ -286,13 +286,13 @@ class FPMULInt2Test extends AnyFlatSpec with ChiselScalatestTester with fpUtils 
       val testCases = Seq.fill(test_num)(
         (
           (random.nextFloat() * 20 - 10), // A: Random float between -10 and 10
-          random.nextInt(3),              // B: Random int between 0 and 15
+          random.nextInt(3) - 1,              // B: Random int between -1 and 1
           (random.nextFloat() * 20 - 10)
         )                                 // C: Random float between -10 and 10
       )
 
       testCases.zipWithIndex.foreach { case ((a, b, c), index) =>
-        test_fp_mul_int(dut, index + 1, a, b, c)
+        test_fp_mul_int(dut, index + 1, a, b, c, 2)
       }
 
     }
@@ -315,25 +315,25 @@ class FPMULInt1Test extends AnyFlatSpec with ChiselScalatestTester with fpUtils 
       var B = 1    // uint4
       var C = 1.0f // float32
 
-      test_fp_mul_int(dut, 1, A, B, C)
+      test_fp_mul_int(dut, 1, A, B, C, 1)
 
       A = 0.5f
       B = 0
       C = 2.3f
 
-      test_fp_mul_int(dut, 2, A, B, C)
+      test_fp_mul_int(dut, 2, A, B, C, 1)
 
       A = 0.005f
       B = 1
       C = 100f
 
-      test_fp_mul_int(dut, 3, A, B, C)
+      test_fp_mul_int(dut, 3, A, B, C, 1)
 
       A = 0.005f
       B = 0
       C = 10f
 
-      test_fp_mul_int(dut, 4, A, B, C)
+      test_fp_mul_int(dut, 4, A, B, C, 1)
 
       val random = new Random()
 
@@ -351,7 +351,7 @@ class FPMULInt1Test extends AnyFlatSpec with ChiselScalatestTester with fpUtils 
       )
 
       testCases.zipWithIndex.foreach { case ((a, b, c), index) =>
-        test_fp_mul_int(dut, index + 1, a, b, c)
+        test_fp_mul_int(dut, index + 1, a, b, c, 1)
       }
 
     }
