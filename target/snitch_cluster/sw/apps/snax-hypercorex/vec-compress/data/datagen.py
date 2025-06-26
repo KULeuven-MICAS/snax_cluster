8dc9b72e281c65a19e1f6bd1ab6bcb6e3c8c7428
rgantonio <ryan.antonio@esat.kuleuven.be>
2025-06-26 14:15:48 +0200

    sw: fmt

--- target/snitch_cluster/sw/apps/snax-hypercorex/vec-compress/data/datagen.py
+++ target/snitch_cluster/sw/apps/snax-hypercorex/vec-compress/data/datagen.py
@@ -20,7 +20,7 @@ np.random.seed(42)
 sys.path.append(
     os.path.join(os.path.dirname(__file__), "../../../../../../../util/sim/")
 )
-from data_utils import format_scalar_definition, format_vector_definition  # noqa: E402
+from data_utils import format_vector_definition  # noqa: E402
 
 # -----------------------
 # Add hypercorex utility paths
