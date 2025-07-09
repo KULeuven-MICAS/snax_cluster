cb409cd9e7dd771d4f98964de0707f8713ab6f2c
Jonas Crols <jonas.crols@telenet.be>
2025-07-09 11:01:47 +0200

    add 3 elementwise addition test

--- hw/chisel/src/test/scala/snax/DataPathExtension/ElementwiseAddTester.scala
+++ hw/chisel/src/test/scala/snax/DataPathExtension/ElementwiseAddTester.scala
@@ -3,7 +3,7 @@ import scala.util.Random
 
 import snax.DataPathExtension.hasElementwiseAdd
 
-class ElementwiseAddTester extends DataPathExtensionTester {
+class ElementwiseAdd2Tester extends DataPathExtensionTester {
 
   def hasExtension = new hasElementwiseAdd(elementWidth = 16, dataWidth = 512)
 
@@ -29,9 +29,44 @@ class ElementwiseAddTester extends DataPathExtensionTester {
     val rightOutputMatrix = rightInputMatrix1.zip(rightInputMatrix2).map { case (a, b) => a.zip(b).map { case (x, y) => x + y } }
     outputData.append(BigInt(leftOutputMatrix.flatten.map { i => f"$i%04X" }.reverse.reduce(_ + _), 16))
     outputData.append(BigInt(rightOutputMatrix.flatten.map { i => f"$i%04X" }.reverse.reduce(_ + _), 16))
-    println(s"Input Matrix $i: ${leftInputMatrix1.map(_.mkString(" ")).mkString("\n")}")
-    println(s"Input Matrix $i: ${leftInputMatrix2.map(_.mkString(" ")).mkString("\n")}")
-    println(s"Output Matrix $i: ${leftOutputMatrix.map(_.mkString(" ")).mkString("\n")}")
+  }
+
+  val input_data_vec = inputData.toSeq
+
+  val output_data_vec = outputData.toSeq
+}
+
+class ElementwiseAdd3Tester extends DataPathExtensionTester {
+
+  def hasExtension = new hasElementwiseAdd(elementWidth = 16, dataWidth = 512)
+
+  val amount_of_vectors = 3
+  val csr_vec = Seq(amount_of_vectors)
+
+  val inputData  = collection.mutable.Buffer[BigInt]()
+  val outputData = collection.mutable.Buffer[BigInt]()
+
+  for (i <- 0 until 128) {
+    val inputMatrix1: Array[Array[Int]] = Array.fill(8, 8)(Random.nextInt(1 << 14))
+    val inputMatrix2: Array[Array[Int]] = Array.fill(8, 8)(Random.nextInt(1 << 14))
+    val inputMatrix3: Array[Array[Int]] = Array.fill(8, 8)(Random.nextInt(1 << 14))
+    val leftInputMatrix1  = inputMatrix1.map(row => row.slice(0, 4))
+    val rightInputMatrix1 = inputMatrix1.map(row => row.slice(4, 8))
+    val leftInputMatrix2  = inputMatrix2.map(row => row.slice(0, 4))
+    val rightInputMatrix2 = inputMatrix2.map(row => row.slice(4, 8))
+    val leftInputMatrix3  = inputMatrix3.map(row => row.slice(0, 4))
+    val rightInputMatrix3 = inputMatrix3.map(row => row.slice(4, 8))
+    inputData.append(BigInt(leftInputMatrix1.flatten.map { i => f"$i%04X" }.reverse.reduce(_ + _), 16))
+    inputData.append(BigInt(leftInputMatrix2.flatten.map { i => f"$i%04X" }.reverse.reduce(_ + _), 16))
+    inputData.append(BigInt(leftInputMatrix3.flatten.map { i => f"$i%04X" }.reverse.reduce(_ + _), 16))
+    inputData.append(BigInt(rightInputMatrix1.flatten.map { i => f"$i%04X" }.reverse.reduce(_ + _), 16))
+    inputData.append(BigInt(rightInputMatrix2.flatten.map { i => f"$i%04X" }.reverse.reduce(_ + _), 16))
+    inputData.append(BigInt(rightInputMatrix3.flatten.map { i => f"$i%04X" }.reverse.reduce(_ + _), 16))
+
+    val leftOutputMatrix = leftInputMatrix1.zip(leftInputMatrix2.zip(leftInputMatrix3)).map { case (a, (b,c)) => a.zip(b.zip(c)).map { case (x, (y, z)) => x + y + z } }
+    val rightOutputMatrix = rightInputMatrix1.zip(rightInputMatrix2.zip(rightInputMatrix3)).map { case (a, (b,c)) => a.zip(b.zip(c)).map { case (x, (y, z)) => x + y + z } }
+    outputData.append(BigInt(leftOutputMatrix.flatten.map { i => f"$i%04X" }.reverse.reduce(_ + _), 16))
+    outputData.append(BigInt(rightOutputMatrix.flatten.map { i => f"$i%04X" }.reverse.reduce(_ + _), 16))
   }
 
   val input_data_vec = inputData.toSeq
