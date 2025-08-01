bf2c56233ab31711aede82f49adc89c0f0236709
Jonas Crols <jonas.crols@telenet.be>
2025-08-01 13:16:28 +0200

    make rescale up work

--- hw/chisel/src/main/scala/snax/DataPathExtension/RescaleUp.scala
+++ hw/chisel/src/main/scala/snax/DataPathExtension/RescaleUp.scala
@@ -2,12 +2,13 @@ package snax.DataPathExtension
 
 import chisel3._
 import chisel3.util._
+import org.w3c.dom.css.Counter
 
 class RescaleUpPE(
   in_elementWidth:  Int,
   out_elementWidth: Int
 ) extends Module {
-  require(in_elementWidth > out_elementWidth)
+  require(out_elementWidth % in_elementWidth == 0, s"RescaleUpPE: out_elementWidth ($out_elementWidth) must be a multiple of in_elementWidth ($in_elementWidth)")
 
   val io = IO(new Bundle {
     val data_i     = Flipped(Valid(SInt(in_elementWidth.W)))
@@ -15,58 +16,180 @@ class RescaleUpPE(
     val input_zp   = Input(SInt(32.W)) // Zero point for input
     val multiplier = Input(UInt(32.W))
     val output_zp  = Input(SInt(32.W))
-    val shift      = Input(UInt(8.W))  // shift can only be 8 bits
+    val shift      = Input(UInt(6.W))  // shift can only be 6 bits
   })
 
-  val zero_compensated_data_i = Wire(SInt(in_elementWidth.W))
+  val zero_compensated_data_i = Wire(SInt((in_elementWidth + 1).W))
   zero_compensated_data_i := io.data_i.bits - io.input_zp
 
-  val multiplied_data_i = Wire(SInt((in_elementWidth + 32).W)) // Length of input data + multiplier
+  val multiplied_data_i = Wire(SInt((in_elementWidth + 32 + 1).W)) // Length of input data + multiplier
   multiplied_data_i := zero_compensated_data_i * Cat(0.U(1.W), io.multiplier)
 
   val shifted_one = Wire(SInt((in_elementWidth + 32).W))
-  shifted_one := (1.S << (io.shift).asUInt).asSInt
+  shifted_one := (1.S << (io.shift - 1.U).asUInt).asSInt
 
   val shifted_data_i = Wire(SInt((in_elementWidth + 32).W))
   shifted_data_i := (multiplied_data_i + shifted_one)
 
-  val scaled_32_data = Wire(SInt((in_elementWidth + 32).W))
-  when(zero_compensated_data_i >= 0.S) {
-    scaled_32_data := shifted_data_i + (1.S << 30).asSInt
-  }.otherwise {
-    scaled_32_data := shifted_data_i - (1.S << 30).asSInt
-  }
-
-  val correct_shift_value = Wire(SInt((in_elementWidth + 32).W))
-  when(io.shift > 31.U) {
-    correct_shift_value := scaled_32_data
-  }.otherwise {
-    correct_shift_value := shifted_data_i
-  }
-
   val shifted_value = Wire(SInt((in_elementWidth + 32).W))
-  shifted_value := correct_shift_value >> io.shift
+  shifted_value := shifted_data_i >> io.shift
 
-  val long_truncated_value = Wire(SInt(in_elementWidth.W))
+  val long_truncated_value = Wire(SInt(out_elementWidth.W))
   long_truncated_value := shifted_value.asSInt
 
-  val zero_compensated_out = Wire(SInt(in_elementWidth.W))
+  val zero_compensated_out = Wire(SInt(out_elementWidth.W))
   zero_compensated_out := long_truncated_value + io.output_zp
 
-  val intervalled_out = Wire(SInt(in_elementWidth.W))
-
-  when(zero_compensated_out >= (1.S << (out_elementWidth - 1).U).asSInt) {
-    intervalled_out := ((1.S << (out_elementWidth - 1).U) - 1.S).asSInt
-  }.elsewhen(zero_compensated_out < -(1.S << (out_elementWidth - 1).U).asSInt) {
-    intervalled_out := -(1.S << (out_elementWidth - 1).U).asSInt
-  }.otherwise {
-    intervalled_out := zero_compensated_out
-  }
-
-  io.data_o := intervalled_out.asSInt
+  io.data_o := zero_compensated_out.asSInt
 }
 
-class HasRescaleUp(in_elementWidth: Int = 32, out_elementWidth: Int = 8) extends HasDataPathExtension {
+object CtrlEnumObj {
+  object CtrlEnum extends ChiselEnum {
+    val Idle, WaitingIdle, Busy0, Busy1, Busy2, Busy3, Waiting = Value
+  }
+}
+
+class RescaleUpCtrl(
+  in_elementWidth:  Int,
+  out_elementWidth: Int
+) extends Module {
+  import CtrlEnumObj.CtrlEnum
+  import CtrlEnumObj.CtrlEnum._
+  require(out_elementWidth % in_elementWidth == 0, s"RescaleUpCtrl: out_elementWidth ($out_elementWidth) must be a multiple of in_elementWidth ($in_elementWidth)")
+
+  val io = IO(new Bundle {
+    val start = Input(Bool())
+    val next_ready = Input(Bool())
+    val previous_valid = Input(Bool())
+    val counter_out = Output(UInt(log2Ceil((out_elementWidth / in_elementWidth)).W))
+    val self_valid = Output(Bool())
+    val self_ready = Output(Bool())
+    val self_busy = Output(Bool())
+  })
+
+
+    val state = RegInit(Idle)
+
+    switch (state) {
+      is (Idle) {
+        when (io.start) {
+          when(io.previous_valid) {
+            state := Busy0
+          }.otherwise {
+            state := WaitingIdle
+          }
+        }.otherwise {
+          state := Idle
+        }
+      }
+      is (WaitingIdle) {
+        when(io.previous_valid) {
+          state := Busy0
+        }.otherwise {
+          state := WaitingIdle
+        }
+      }
+      is (Busy0) {
+        when (io.next_ready) {
+          state := Busy1
+        }.otherwise {
+          state := Busy0
+        }
+      }
+      is (Busy1) {
+        when (io.next_ready) {
+          state := Busy2
+        }.otherwise {
+          state := Busy1
+        }
+      }
+      is (Busy2) {
+        when (io.next_ready) {
+          state := Busy3
+        }.otherwise {
+          state := Busy2
+        }
+      }
+      is (Busy3) {
+        when (io.next_ready) {
+          when (io.previous_valid) {
+            state := Busy0
+          }.otherwise {
+            state := Waiting
+          }
+        }.otherwise {
+          state := Busy3
+        }
+      }
+      is (Waiting) {
+        when (io.previous_valid) {
+          state := Busy0
+        }.otherwise {
+          state := Waiting
+        }
+      }
+    } //TODO: make logic for when machine is done and should somehow return to initial state
+
+    io.counter_out := DontCare
+    io.self_valid := false.B
+    io.self_ready := false.B
+    io.self_busy := false.B
+
+    switch (state) {
+      is (Idle) {
+        io.counter_out := DontCare
+        io.self_valid := false.B
+        io.self_ready := false.B
+        io.self_busy := false.B
+      }
+      is (WaitingIdle) {
+        io.counter_out := DontCare
+        io.self_valid := false.B
+        io.self_ready := false.B
+        io.self_busy := true.B
+      }
+      is (Busy0) {
+        io.counter_out := 0.U
+        io.self_valid := true.B
+        io.self_ready := false.B
+        io.self_busy := true.B
+      }
+      is (Busy1) {
+        io.counter_out := 1.U
+        io.self_valid := true.B
+        io.self_ready := false.B
+        io.self_busy := true.B
+      }
+      is (Busy2) {
+        io.counter_out := 2.U
+        io.self_valid := true.B
+        io.self_ready := false.B
+        io.self_busy := true.B
+      }
+      is (Busy3) {
+        io.counter_out := 3.U
+        io.self_valid := true.B
+        io.self_busy := true.B
+        when (io.next_ready) {
+          io.self_ready := true.B
+        }.otherwise {
+          io.self_ready := false.B
+        }
+      }
+      is (Waiting) {
+        io.counter_out := DontCare
+        io.self_valid := false.B
+        io.self_ready := true.B
+        io.self_busy := true.B
+      }
+    }
+
+    dontTouch(state)
+    dontTouch(io.start)
+
+}
+
+class HasRescaleUp(in_elementWidth: Int = 8, out_elementWidth: Int = 32) extends HasDataPathExtension {
   implicit val extensionParam:          DataPathExtensionParam =
     new DataPathExtensionParam(
       moduleName = "RescaleUp",
@@ -93,58 +216,45 @@ class RescaleUp(
   )
 
   require(
-    in_elementWidth % out_elementWidth == 0,
+    out_elementWidth % in_elementWidth == 0,
     s"RescaleUp: in_elementWidth ($in_elementWidth) must be a multiple of out_elementWidth ($out_elementWidth)"
   )
 
-  val counter = Module(new snax.utils.BasicCounter(log2Ceil(in_elementWidth / out_elementWidth)) {
-    override val desiredName = "MaxPoolCounter"
-  })
-  counter.io.ceil := (in_elementWidth / out_elementWidth).asUInt
-  counter.io.reset := ext_start_i
-  counter.io.tick  := ext_data_i.fire
-  ext_busy_o       := counter.io.value =/= ((in_elementWidth / out_elementWidth).U - 1.U)
+  // Control Logic
+  val ctrl = Module(new RescaleUpCtrl(in_elementWidth, out_elementWidth))
+  ctrl.io.start := io.start_i
+  ctrl.io.next_ready := ext_data_o.ready
+  ctrl.io.previous_valid := ext_data_i.valid
+  val counter_val = WireInit(ctrl.io.counter_out.asUInt)
+  ext_data_o.valid := ctrl.io.self_valid
+  ext_busy_o := ctrl.io.self_busy
+  ext_data_i.ready := ctrl.io.self_ready
+  dontTouch(ctrl.io.counter_out)
 
+  // Csr Logic for RescaleUp
   val input_zp   = WireInit(ext_csr_i(0).asSInt)
   val multiplier = WireInit(ext_csr_i(1).asUInt)
   val output_zp  = WireInit(ext_csr_i(2).asSInt)
   val shift      = WireInit(ext_csr_i(3).asUInt)
 
-  val regs = RegInit(
-    VecInit(
-      Seq.fill((extensionParam.dataWidth / out_elementWidth) - (extensionParam.dataWidth / in_elementWidth))(
-        0.S(out_elementWidth.W)
-      )
-    )
-  )
+  // Data Logic
+  val input_vec = WireInit(ext_data_i.bits.asTypeOf(Vec(extensionParam.dataWidth / in_elementWidth, SInt(in_elementWidth.W))))
+  val output_vec = Wire(Vec(extensionParam.dataWidth / out_elementWidth, SInt(out_elementWidth.W)))
 
-  val out_wires = Wire(Vec(extensionParam.dataWidth / in_elementWidth, SInt(out_elementWidth.W)))
-
-  val PEs = for (i <- 0 until extensionParam.dataWidth / in_elementWidth) yield {
+  val PEs = for (i <- 0.until(extensionParam.dataWidth / out_elementWidth)) yield {
     val PE = Module(new RescaleUpPE(in_elementWidth = in_elementWidth, out_elementWidth = out_elementWidth) {
       // override val desiredName = "RescaleUpPE"
     })
-    PE.io.data_i.valid := ext_data_i.fire
-    PE.io.data_i.bits  := ext_data_i
-      .bits(
-        (i + 1) * in_elementWidth - 1,
-        i * in_elementWidth
-      )
-      .asSInt
+    PE.io.data_i.valid := 1.U(1.W)
+    PE.io.data_i.bits  := input_vec(counter_val * (extensionParam.dataWidth / out_elementWidth).U + i.U)
     PE.io.input_zp     := input_zp.asSInt
     PE.io.multiplier   := multiplier.asUInt
     PE.io.output_zp    := output_zp.asSInt
     PE.io.shift        := shift.asUInt
 
-    when(ext_data_i.fire) {
-      when(counter.io.value =/= ((in_elementWidth / out_elementWidth).U - 1.U)) {
-        regs(counter.io.value * (extensionParam.dataWidth / in_elementWidth).U + i.U) := PE.io.data_o.asSInt
-      }
-    }
-    out_wires(i) := PE.io.data_o.asSInt
+    output_vec(i) := PE.io.data_o.asSInt
   }
 
-  ext_data_o.bits  := VecInit(regs ++ out_wires).asTypeOf(ext_data_o.bits)
-  ext_data_o.valid := ext_data_i.fire && counter.io.value === ((in_elementWidth / out_elementWidth).U - 1.U)
-  ext_data_i.ready := ext_data_o.ready // Check if this can be more efficient
+  // Output Logic
+  ext_data_o.bits := output_vec.asTypeOf(UInt(extensionParam.dataWidth.W))
 }
