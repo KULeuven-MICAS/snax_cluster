3aec1d34deb40f3cfc1b1ead190d109a17797cee
rgantonio <ryan.antonio@esat.kuleuven.be>
2025-08-04 23:54:56 +0200

    sw: add successful initial tiling

--- target/snitch_cluster/sw/apps/snax-hypercorex/isolet-recog-long/src/isolet-recog-long.c
+++ target/snitch_cluster/sw/apps/snax-hypercorex/isolet-recog-long/src/isolet-recog-long.c
@@ -77,15 +77,13 @@ int main() {
         snrt_dma_wait_all();
 
         uint32_t end_dma_load = snrt_mcycle();
-        printf("DMA load time: %d\n", end_dma_load - start_dma_load);
+        printf("DMT: %d\n", end_dma_load - start_dma_load);
     };
 
     // Synchronize cores
     snrt_cluster_hw_barrier();
 
-    //-------------------------------
-    // Set everything for the compute core
-    //-------------------------------
+    // In parallel do the computations already
     if (snrt_is_compute_core()) {
         //-------------------------------
         // Configuring the streamers
@@ -157,7 +155,7 @@ int main() {
         csrw_ss(HYPERCOREX_CORE_SET_REG_ADDR, 0x00000008);
 
         uint32_t core_config_end = snrt_mcycle();
-        printf("Core config time: %d\n", core_config_end - core_config_start);
+        printf("CGT: %d\n", core_config_end - core_config_start);
 
         uint32_t core_start = snrt_mcycle();
 
@@ -170,7 +168,125 @@ int main() {
         };
 
         uint32_t core_end = snrt_mcycle();
-        printf("Core run time: %d\n", core_end - core_start);
+        printf("CRT: %d\n", core_end - core_start);
+
+        // Clear the HDC core... (so stupid)
+        csrw_ss(HYPERCOREX_CORE_SET_REG_ADDR, 0x00000040);
+
+        //-------------------------------
+        // Check results
+        //-------------------------------
+        for (uint32_t i = 0; i < num_predictions; i++) {
+            if (golden_list_data[i] != (uint32_t) * (predict_start + i * 64)) {
+                err++;
+            }
+        };
+    };
+
+    snrt_cluster_hw_barrier();
+
+    
+    // Transfer again to 2nd start
+    if (snrt_is_dm_core()) {
+        uint32_t start_dma_load = snrt_mcycle();
+
+        // Load the data a list into the 2nd 8 banks
+        snrt_dma_start_2d(
+            // Destination address, source address
+            data_start_2, test_samples_list,
+            // Size per chunk
+            src_stride,
+            // Destination stride, source stride
+            dst_stride, src_stride,
+            // Number of times to do
+            num_rows);
+
+        // Ensure that all DMA tasks finish
+        snrt_dma_wait_all();
+
+        uint32_t end_dma_load = snrt_mcycle();
+        printf("DMT: %d\n", end_dma_load - start_dma_load);
+    };
+
+    snrt_cluster_hw_barrier();
+
+    if (snrt_is_compute_core()) {
+        //-------------------------------
+        // Configuring the streamers
+        //-------------------------------
+        uint32_t core_config_start = snrt_mcycle();
+        // Configure streamer for high dim A
+        hypercorex_set_streamer_lowdim_a(
+            (uint32_t)data_start_2,  // Base pointer low
+            0,                       // Base pointer high
+            1,                       // Spatial stride
+            8,                       // Inner loop bound
+            num_rows,                // Outer loop bound
+            8,                       // Inner loop stride
+            256                      // Outer loop stride
+        );
+
+        // No need to re-configure AM
+        // Therefore we leave it out
+
+        hypercorex_set_streamer_lowdim_predict(
+            (uint32_t)predict_start,  // Base pointer low
+            0,                        // Base pointer high
+            1,                        // Spatial stride
+            num_predictions,          // Inner loop bound
+            1,                        // Outer loop bound
+            256,                      // Inner loop stride
+            0                         // Outer loop stride
+        );
+
+        // Start the streamers
+        hypercorex_start_streamer();
+
+        // -------------------------------
+        // Configuring the Hypercorex
+        // -------------------------------
+
+        // Write number of classes to be checked
+        csrw_ss(HYPERCOREX_AM_NUM_PREDICT_REG_ADDR, num_classes);
+
+        // Load instructions for hypercorex
+        hypercorex_load_inst(4, 0, code);
+
+        // Enable loop mode to 2D
+        csrw_ss(HYPERCOREX_INST_LOOP_CTRL_REG_ADDR, 0x00000002);
+
+        // Encoding loops and jumps
+        hypercorex_set_inst_loop_jump_addr(0, 0, 0);
+
+        hypercorex_set_inst_loop_end_addr(0, 3, 0);
+
+        hypercorex_set_inst_loop_count(num_features, num_predictions, 0);
+
+        // Set data slice control and automatic updates
+        hypercorex_set_data_slice_ctrl(3, 0, 0, 1);
+        hypercorex_set_data_slice_num_elem_a(num_features);
+
+        hypercorex_set_auto_counter_start_b(0);
+        hypercorex_set_auto_counter_num_b(num_features);
+
+        // Write control registers
+        csrw_ss(HYPERCOREX_CORE_SET_REG_ADDR, 0x00000008);
+
+        uint32_t core_config_end = snrt_mcycle();
+        printf("CGT: %d\n", core_config_end - core_config_start);
+
+        uint32_t core_start = snrt_mcycle();
+    
+        // Start hypercorex
+        csrw_ss(HYPERCOREX_CORE_SET_REG_ADDR, 0x00000009);
+
+        // Poll the busy-state of Hypercorex
+        // Check both the Hypercorex and Streamer
+        while (csrr_ss(STREAMER_BUSY_CSR)) {
+        };
+
+        uint32_t core_end = snrt_mcycle();
+        printf("CRT: %d\n", core_end - core_start);
 
         //-------------------------------
         // Check results
