69fd60f013d28d75df5d237fe76c3b273b465e21
Jonas Crols <jonas.crols@telenet.be>
2025-08-12 10:16:39 +0200

    make all other apps work again

--- target/snitch_cluster/sw/apps/snax-xdma-transpose/src/snax-xdma-transpose.c
+++ target/snitch_cluster/sw/apps/snax-xdma-transpose/src/snax-xdma-transpose.c
@@ -45,6 +45,16 @@ int main() {
             err++;
         }
 
+        if (xdma_disable_src_ext(3) != 0) {
+            printf("Error in disabling reader xdma extension 3\n");
+            err++;
+        }
+
+        if (xdma_disable_src_ext(4) != 0) {
+            printf("Error in disabling reader xdma extension 4\n");
+            err++;
+        }
+
         if (xdma_disable_dst_ext(0) != 0) {
             printf("Error in disabling writer xdma extension 1\n");
             err++;
