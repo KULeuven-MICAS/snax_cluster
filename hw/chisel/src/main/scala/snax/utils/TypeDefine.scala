e4689deb5bf1c4ad64a5490af160765d91c61c61
Yunhao Deng <yunhao.deng@kuleuven.be>
2025-09-05 14:50:16 +0200

    Incorporate Ryan's suggestion

--- hw/chisel/src/main/scala/snax/utils/TypeDefine.scala
+++ hw/chisel/src/main/scala/snax/utils/TypeDefine.scala
@@ -13,15 +13,3 @@ class RegReq(addrWidth: Int, tcdmDataWidth: Int) extends Bundle {
 class RegRsp(tcdmDataWidth: Int) extends Bundle {
   val data = UInt(tcdmDataWidth.W)
 }
-
-// CSR read/write cmd (same with TCDM request, but fixed 32 bit data width and 4 bit strobe)
-// class CsrReq(addrWidth: Int) extends Bundle {
-//   val addr  = UInt(addrWidth.W)
-//   val write = Bool()
-//   val data  = UInt(32.W)
-//   val strb  = UInt(4.W)
-// }
-
-// class CsrRsp extends Bundle {
-//   val data = UInt(32.W)
-// }
