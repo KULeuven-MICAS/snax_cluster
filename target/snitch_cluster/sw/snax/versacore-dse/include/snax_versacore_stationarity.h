a1ebbc1952cae96ef95058cc8ea3904705775327
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2025-11-18 20:46:54 +0100

    add the dse hw and sw

---
+++ target/snitch_cluster/sw/snax/versacore-dse/include/snax_versacore_stationarity.h
@@ -0,0 +1,7 @@
+// Copyright 2024 KU Leuven.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Xiaoling Yi <xiaoling.yi@esat.kuleuven.be>
+
+#define SNAX_VERSACORE_OUTPUT_STATIONARY_ONLY
