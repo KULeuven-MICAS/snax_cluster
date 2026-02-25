70c8900d1ea42a8b84a1f83758d8333142828768
Yunhao Deng <yunhao.deng@kuleuven.be>
2026-02-25 14:46:52 +0100

    scalafmt

--- hw/chisel/src/main/scala/snax/utils/TypeDefine.scala
+++ hw/chisel/src/main/scala/snax/utils/TypeDefine.scala
@@ -4,10 +4,10 @@ import chisel3._
 
 // simplified tcdm interface
 class RegReq(addrWidth: Int, dataWidth: Int) extends Bundle {
-  val addr     = UInt(addrWidth.W)
-  val write    = Bool()
-  val data     = UInt(dataWidth.W)
-  val strb     = UInt((dataWidth / 8).W)
+  val addr  = UInt(addrWidth.W)
+  val write = Bool()
+  val data  = UInt(dataWidth.W)
+  val strb  = UInt((dataWidth / 8).W)
 }
 
 class RegRsp(dataWidth: Int) extends Bundle {
