16144a69df73a5990ab35e7d270712b9514d9b0c
Yunhao Deng <yunhao.deng@kuleuven.be>
2026-05-25 15:45:34 +0200

    New impl. and testbench for elementwiseadd

--- hw/chisel/src/main/scala/snax/DataPathExtension/ElementwiseAdd.scala
+++ hw/chisel/src/main/scala/snax/DataPathExtension/ElementwiseAdd.scala
@@ -34,49 +34,52 @@ class ElementwiseAdd(
     s"Data width ${extensionParam.dataWidth} must be a multiple of element width $elementWidth"
   )
 
+  private val elementPerVector = extensionParam.dataWidth / elementWidth
+
   // Counter to record the steps
   val counter = Module(new snax.utils.BasicCounter(8) {
     override val desiredName = "ElementwiseAddCounter"
   })
-  counter.io.ceil := ext_csr_i(0)
+
+  val operandCount = ext_csr_i(0)(7, 0)
+  counter.io.ceil := operandCount
   counter.io.reset := ext_start_i
   counter.io.tick  := ext_data_i.fire
-  ext_busy_o       := counter.io.value =/= 0.U
 
-  // The wire to connect the output result
-  val ext_data_o_bits = Wire(
-    Vec(extensionParam.dataWidth / elementWidth, UInt(elementWidth.W))
-  )
-
-  // The register to hold the sum of each Element
+  // The register holds the partial sum and then the completed output.
   val regs = RegInit(
     VecInit(
-      Seq.fill(extensionParam.dataWidth / elementWidth)(0.U(elementWidth.W))
+      Seq.fill(elementPerVector)(0.U(elementWidth.W))
     )
   )
+  val outputValid = RegInit(false.B)
 
   val input_data = ext_data_i.bits.asTypeOf(
-    Vec(extensionParam.dataWidth / elementWidth, UInt(elementWidth.W))
+    Vec(elementPerVector, UInt(elementWidth.W))
   )
-  for (i <- 0 until extensionParam.dataWidth / elementWidth) {
-    ext_data_o_bits(i) := regs(i) + input_data(i)
+
+  val nextSum = Wire(Vec(elementPerVector, UInt(elementWidth.W)))
+  for (i <- 0 until elementPerVector) {
+    nextSum(i) := Mux(counter.io.value === 0.U, input_data(i), regs(i) + input_data(i))
   }
 
-  when(ext_data_i.fire) {
-    when(counter.io.value === (ext_csr_i(0) - 1.U(8.W))) {
-      // Reset the registers when the counter reaches the end
-      for (i <- 0 until extensionParam.dataWidth / elementWidth) {
-        regs(i) := 0.U
-      }
-    } otherwise {
-      // Add the input data to the corresponding register
-      for (i <- 0 until extensionParam.dataWidth / elementWidth) {
-        regs(i) := regs(i) + input_data(i)
-      }
-    }
+  val lastInput  = counter.io.value === (operandCount - 1.U)
+  val outputFire = ext_data_o.fire
+  val inputFire  = ext_data_i.fire
+
+  when(ext_start_i) {
+    regs        := VecInit(Seq.fill(elementPerVector)(0.U(elementWidth.W)))
+    outputValid := false.B
+  } .elsewhen(inputFire) {
+    regs        := nextSum
+    outputValid := lastInput
+  } .elsewhen(outputFire) {
+    regs        := VecInit(Seq.fill(elementPerVector)(0.U(elementWidth.W)))
+    outputValid := false.B
   }
 
-  ext_data_o.bits  := Cat(ext_data_o_bits.reverse)
-  ext_data_o.valid := (counter.io.value === (ext_csr_i(0) - 1.U)(7, 0)) & ext_data_i.fire
-  ext_data_i.ready := ext_data_o.ready
+  ext_data_o.bits  := Cat(regs.reverse)
+  ext_data_o.valid := outputValid
+  ext_data_i.ready := !outputValid || ext_data_o.ready
+  ext_busy_o       := (counter.io.value =/= 0.U) || outputValid
 }
