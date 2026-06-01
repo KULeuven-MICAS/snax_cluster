d9a3a29b1244feeaace0459ae6ef92b77ed07ac2
Yunhao Deng <IveanEx@outlook.com>
2026-06-01 15:35:55 +0200

    Add authorship information to snax-xdma-elementwise-add.c

--- target/snitch_cluster/sw/apps/snax-xdma-elementwise-add/src/snax-xdma-elementwise-add.c
+++ target/snitch_cluster/sw/apps/snax-xdma-elementwise-add/src/snax-xdma-elementwise-add.c
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 //
+// Jonas Crols <jonas.crols@student.kuleuven.be>
 // Yunhao Deng <yunhao.deng@kuleuven.be>
 
 #include "data.h"
