f07f5ae13adb2ae32a6457f212fa32e29ac2b1e8
Konste11ation <fanchen.kong@outlook.be>
2026-07-09 16:39:01 +0200

    Restore scalafmt-clean StreamParamGen.scala
    StreamParamGen.scala is regenerated by util/snaxgen/snaxgen.py on every
    `make rtl-gen`, and raw snaxgen output is not scalafmt-formatted (trailing
    whitespace, unaligned `=`). A gemmX-config rtl-gen output had been committed,
    which turned the "Lint Scala Files" CI check red.
    
    Restore the file to the scalafmt-clean version tracked on main. The file's
    contents are irrelevant to CI correctness -- the Build and Test job rewrites
    it via rtl-gen before `sbt test` -- only its formatting matters, because the
    lint job checks the committed copy without running rtl-gen.

--- hw/chisel/src/main/scala/snax/streamer/StreamParamGen.scala
+++ hw/chisel/src/main/scala/snax/streamer/StreamParamGen.scala
@@ -2,13 +2,12 @@
 // Solderpad Hardware License, Version 0.51, see LICENSE for details.
 // SPDX-License-Identifier: SHL-0.51
 
-
 package snax.streamer
- 
+
 import snax.readerWriter._
 import snax.reqRspManager._
 import snax.utils._
- 
+
 import chisel3._
 import chisel3.util._
 
@@ -19,117 +18,72 @@ object StreamerParametersGen {
 // constrain: all the reader and writer needs to have same config of crossClockDomain
   def hasCrossClockDomain = false
 
-  def readerParams = Seq(
-    new ReaderWriterParam(
-      spatialBounds = List(
-        8
+  def readerParams =
+    Seq(
+      new ReaderWriterParam(
+        spatialBounds       = List(
+          16
+        ),
+        temporalDimension   = 6,
+        tcdmDataWidth       = 64,
+        tcdmSize            = 256,
+        tcdmLogicWordSize   = Seq(256),
+        numChannel          = 16,
+        addressBufferDepth  = 2,
+        dataBufferDepth     = 2,
+        configurableChannel = true,
+        crossClockDomain    = hasCrossClockDomain
       ),
-      temporalDimension = 6,
-      tcdmDataWidth = 64,
-      tcdmSize = 128,
-      tcdmLogicWordSize = Seq(
-        256,
-        128,
-        64
+      new ReaderWriterParam(
+        spatialBounds       = List(
+          128
+        ),
+        temporalDimension   = 3,
+        tcdmDataWidth       = 64,
+        tcdmSize            = 256,
+        tcdmLogicWordSize   = Seq(256),
+        numChannel          = 128,
+        addressBufferDepth  = 2,
+        dataBufferDepth     = 2,
+        configurableChannel = true,
+        crossClockDomain    = hasCrossClockDomain
       ),
-      numChannel = 8,
-      addressBufferDepth = 8,
-      dataBufferDepth = 8,
-      configurableChannel = false,
-      dynamicPriority = true,
-      higherStaticPriority = false,
-      crossClockDomain = hasCrossClockDomain
-   ), 
-    new ReaderWriterParam(
-      spatialBounds = List(
-        8
-      ),
-      temporalDimension = 3,
-      tcdmDataWidth = 64,
-      tcdmSize = 128,
-      tcdmLogicWordSize = Seq(
-        256,
-        128,
-        64
-      ),
-      numChannel = 8,
-      addressBufferDepth = 8,
-      dataBufferDepth = 8,
-      configurableChannel = false,
-      dynamicPriority = true,
-      higherStaticPriority = false,
-      crossClockDomain = hasCrossClockDomain
+      new ReaderWriterParam(
+        spatialBounds       = List(
+          64
+        ),
+        temporalDimension   = 4,
+        tcdmDataWidth       = 64,
+        tcdmSize            = 256,
+        tcdmLogicWordSize   = Seq(256),
+        numChannel          = 64,
+        addressBufferDepth  = 2,
+        dataBufferDepth     = 2,
+        configurableChannel = true,
+        crossClockDomain    = hasCrossClockDomain
+      )
     )
-  )
 
-  def writerParams = Seq(
-    new ReaderWriterParam(
-      spatialBounds = List(
-        8
-      ),
-      temporalDimension = 3,
-      tcdmDataWidth = 64,
-      tcdmSize = 128,
-      tcdmLogicWordSize = Seq(
-        256,
-        128,
-        64
-      ),
-      numChannel = 8,
-      addressBufferDepth = 1,
-      dataBufferDepth = 1,
-      configurableChannel = false,
-      dynamicPriority = true,
-      higherStaticPriority = false,
-      crossClockDomain = hasCrossClockDomain
+  def writerParams =
+    Seq(
+      new ReaderWriterParam(
+        spatialBounds       = List(
+          64
+        ),
+        temporalDimension   = 4,
+        tcdmDataWidth       = 64,
+        tcdmSize            = 256,
+        tcdmLogicWordSize   = Seq(256),
+        numChannel          = 64,
+        addressBufferDepth  = 2,
+        dataBufferDepth     = 2,
+        configurableChannel = true,
+        crossClockDomain    = hasCrossClockDomain
+      )
     )
-  )
 
-  def readerWriterParams = Seq(
-    new ReaderWriterParam(
-      spatialBounds = List(
-        8,
-        4
-      ),
-      temporalDimension = 3,
-      tcdmDataWidth = 64,
-      tcdmSize = 128,
-      tcdmLogicWordSize = Seq(
-        256,
-        128,
-        64
-      ),
-      numChannel = 32,
-      addressBufferDepth = 1,
-      dataBufferDepth = 1,
-      configurableChannel = true,
-      dynamicPriority = true,
-      higherStaticPriority = false,
-      crossClockDomain = hasCrossClockDomain
-   ), 
-    new ReaderWriterParam(
-      spatialBounds = List(
-        8,
-        4
-      ),
-      temporalDimension = 3,
-      tcdmDataWidth = 64,
-      tcdmSize = 128,
-      tcdmLogicWordSize = Seq(
-        256,
-        128,
-        64
-      ),
-      numChannel = 32,
-      addressBufferDepth = 1,
-      dataBufferDepth = 1,
-      configurableChannel = false,
-      dynamicPriority = true,
-      higherStaticPriority = false,
-      crossClockDomain = hasCrossClockDomain
-    )
-  )
+  def readerWriterParams = Seq()
 
-  def tagName = "snax_streamer_gemmX_"
-  def headerFilepath = "../../target/snitch_cluster/sw/snax/gemmx/include"
+  def tagName        = "snax_versacore_"
+  def headerFilepath = "../../target/snitch_cluster/sw/snax/versacore/include"
 }
